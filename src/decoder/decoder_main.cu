14a122c58c874dcfa20c717afc8a068f6fe27c3d
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-04-19 23:00:03 +0200

    more clean=up

--- src/decoder/decoder_main.cu
+++ src/decoder/decoder_main.cu
@@ -3,44 +3,41 @@
 #include <string>
 #include <boost/timer/timer.hpp>
 
-#include "dl4mt.h"
-#include "vocab.h"
+#include "god.h"
 #include "search.h"
 #include "threadpool.h"
 #include "printer.h"
 
 int main(int argc, char* argv[]) {
   God::Init(argc, argv);
-  
-  std::cerr << "Translating...\n";
   std::ios_base::sync_with_stdio(false);
   boost::timer::cpu_timer timer;
   
-  ThreadPool pool(God::Get<size_t>("threads"));
-  std::vector<std::future<History>> results;
+  std::cerr << "Translating...\n";
   
   std::string in;
-  std::size_t threadCounter = 0;
+  std::size_t taskCounter = 0;
+  
+  ThreadPool pool(God::Get<size_t>("threads"));
+  std::vector<std::future<History>> results;
   while(std::getline(std::cin, in)) {
-    auto call = [=] {
+    
+    auto translationTask = [taskCounter, &in]{
       thread_local std::unique_ptr<Search> search;
       if(!search)
-        search.reset(new Search(threadCounter));
+        search.reset(new Search(taskCounter));
       return search->Decode(God::GetSourceVocab()(in));
     };
-  
-    results.emplace_back(
-      pool.enqueue(call)
-    );
-    threadCounter++;
+    
+    results.emplace_back(pool.enqueue(translationTask));
+    
+    taskCounter++;
   }
   
   size_t lineCounter = 0;
-  for(auto&& result : results) {
-    History history = result.get();
-    Printer(history, lineCounter, std::cout);
-    lineCounter++;
-  }
+  for(auto&& result : results)
+    Printer(result.get(), lineCounter++, std::cout);
+
   std::cerr << timer.format() << std::endl;
   return 0;
 }
