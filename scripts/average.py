3cd9a333f0caf87c87027952c81ac93ac569ca7b
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-04-24 11:28:27 +0200

    geometric average

--- scripts/average.py
+++ scripts/average.py
@@ -5,16 +5,16 @@ import numpy as np;
 
 average = dict()
 
+n = len(argv[1:-1])
 for filename in sys.argv[1:-1]:
+    print "Loading", filename 
     with open(filename, "rb") as mfile:
         m = np.load(mfile)
         for k in m:
             if k not in average:
-                average[k] = m[k]
+                average[k] = m[k] ** 1.0/n
             elif average[k].shape == m[k].shape:
-                average[k] += m[k]
+                average[k] *= m[k] ** 1.0/n
 
-for k in average:
-    average[k] /= len(sys.argv[1:-1])
-
+print "Saving to", sys.argv[-1]
 np.savez(sys.argv[-1], **average)
