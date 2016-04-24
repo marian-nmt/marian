549d38c1ceecc29cc29b7c2e795429d1de53881e
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-04-24 11:51:00 +0200

    harmonic

--- scripts/average.py
+++ scripts/average.py
@@ -12,9 +12,12 @@ for filename in sys.argv[1:-1]:
         m = np.load(mfile)
         for k in m:
             if k not in average:
-                average[k] = m[k] ** 1.0/n
+                average[k] = 1 / m[k]
             elif average[k].shape == m[k].shape:
-                average[k] *= m[k] ** 1.0/n
+                average[k] += 1 / m[k]
+
+for k in average:
+    average[k] = n / average[k]
 
 print "Saving to", sys.argv[-1]
 np.savez(sys.argv[-1], **average)
