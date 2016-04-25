36a385e874caf84f44126ffd55c01c5899eefb42
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-04-25 17:02:47 +0200

    options for average.py

--- scripts/average.py
+++ scripts/average.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 
-import sys
+import os, sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--model', nargs='+', required=True,
+                    help="Models to average")
+parser.add_argument('-o', '--output', required=True,
+                    help="Output path")
+args = parser.parse_args()
+
 import numpy as np;
 
 average = dict()
-
-n = len(sys.argv[1:-1])
-for filename in sys.argv[1:-1]:
+n = len(args.models)
+for filename in args.models:
     print "Loading", filename 
     with open(filename, "rb") as mfile:
         m = np.load(mfile)
         for k in m:
-            if k not in average:
-                average[k] = m[k] / n
-            elif average[k].shape == m[k].shape:
-                average[k] += m[k] / n
+            if k != "history_errs":
+                if k not in average:
+                    average[k] = m[k] / n
+                elif average[k].shape == m[k].shape:
+                    average[k] += m[k] / n
+                
 
-print "Saving to", sys.argv[-1]
-np.savez(sys.argv[-1], **average)
+print "Saving to", args.output
+np.savez(args.output, **average)
