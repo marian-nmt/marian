437863be388925cdbc0ab9bbb020af6230d9634e
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-05-03 23:17:09 +0100

    in loop

--- src/test.cpp
+++ src/test.cpp
@@ -11,6 +11,7 @@ int main(int argc, char** argv) {
         size_t max = rand() % 20 + 1;
         
         Var x0 = 1, x1 = 2, x2 = 3;
+        std::vector<Var> x = { x0, x1, x2 };
         
         Var y = 0.0;
         for(int i = 0; i < max; i++) {
@@ -19,7 +20,6 @@ int main(int argc, char** argv) {
             x.push_back(xi);
         }
         
-        std::vector<Var> x = { x0, x1, x2 };
            
         set_zero_all_adjoints();
         y.grad();
