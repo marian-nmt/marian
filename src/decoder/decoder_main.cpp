1260084f585d8d8709d84a9444d9aef1c2dfa7bb
Hieu Hoang <hieuhoang@gmail.com>
2016-07-04 13:09:25 +0100

    compile error on non-osx

--- src/decoder/decoder_main.cpp
+++ src/decoder/decoder_main.cpp
@@ -18,16 +18,21 @@ History TranslationTask(const std::string& in, size_t taskCounter) {
   #ifdef __APPLE__
     static boost::thread_specific_ptr<Search> s_search;
     Search *search = s_search.get();
+
+    if(search == NULL) {
+      LOG(info) << "Created Search for thread " << std::this_thread::get_id();
+      search = new Search(taskCounter);
+      s_search.reset(search);
+    }
   #else
     thread_local std::unique_ptr<Search> search;
+
+    if(!search) {
+      LOG(info) << "Created Search for thread " << std::this_thread::get_id();
+      search.reset(new Search(taskCounter));
+    }
   #endif
 
-  if(search == NULL) {
-    LOG(info) << "Created Search for thread " << std::this_thread::get_id();
-	search = new Search(taskCounter);
-    s_search.reset(search);
-  }
-  
   return search->Decode(Sentence(taskCounter, in));  
 }
 
