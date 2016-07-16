764285c51cbfc84f514518bedacf994de99b418a
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-07-16 22:16:16 +0200

    Revert "remove slow gru"
    This reverts commit 2f574fd2e74d2caff56ddb895e3b225c500cdb37.

--- src/dl4mt/gru.h
+++ src/dl4mt/gru.h
@@ -2,7 +2,75 @@
 #include "mblas/matrix.h"
 
 template <class Weights>
-class GRU {
+class SlowGRU {
+  public:
+    SlowGRU(const Weights& model)
+    : w_(model) {}
+          
+    void GetNextState(mblas::Matrix& NextState,
+                      const mblas::Matrix& State,
+                      const mblas::Matrix& Context) const {
+      using namespace mblas;
+      namespace bpp = boost::phoenix::placeholders;
+      
+      const size_t cols = GetStateLength();
+      
+      // @TODO: Optimization
+      // @TODO: Launch streams to perform GEMMs in parallel
+      // @TODO: Join matrices and perform single GEMM --------
+      Prod(RU_, Context, w_.W_);
+      Prod(H_,  Context, w_.Wx_);
+      // -----------------------------------------------------
+      
+      // @TODO: Join matrices and perform single GEMM --------
+      Prod(Temp1_, State, w_.U_);
+      Prod(Temp2_, State, w_.Ux_);        
+      // -----------------------------------------------------
+      
+      // @TODO: Organize into one kernel ---------------------
+      BroadcastVec(bpp::_1 + bpp::_2, RU_, w_.B_); // Broadcasting row-wise
+      Element(Logit(bpp::_1 + bpp::_2), RU_, Temp1_);
+      Slice(R_, RU_, 0, cols);
+      Slice(U_, RU_, 1, cols);
+      
+      BroadcastVec(bpp::_1 + bpp::_2, H_,    w_.Bx1_); // Broadcasting row-wise
+      BroadcastVec(bpp::_1 + bpp::_2, Temp2_, w_.Bx2_); // Broadcasting row-wise
+      
+      Element(Tanh(bpp::_1 + bpp::_2 * bpp::_3), H_, R_, Temp2_);
+      Element((1.0 - bpp::_1) * bpp::_2 + bpp::_1 * bpp::_3, U_, H_, State);
+      // -----------------------------------------------------
+      
+      NextState.swap(U_);
+    }
+    
+    size_t GetStateLength() const {
+      return w_.U_.rows();
+    }
+    
+  private:
+    // Model matrices
+    const Weights& w_;
+    
+    // reused to avoid allocation
+    mutable mblas::Matrix RU_;
+    mutable mblas::Matrix R_;
+    mutable mblas::Matrix U_;
+    mutable mblas::Matrix H_;
+    mutable mblas::Matrix Temp1_;
+    mutable mblas::Matrix Temp2_;
+};
+
+void gElementwiseOps(float* out,
+                    const float* state,
+                    const float* ruh,
+                    const float* t,
+                    const float* b,
+                    const float* bx1,
+                    const float* bx2,
+                    size_t rows, size_t cols);
+
+template <class Weights>
+class FastGRU {
   public:
     FastGRU(const Weights& model)
     : w_(model) {
@@ -60,3 +128,6 @@ class GRU {
     mutable mblas::Matrix RUH_;
     mutable mblas::Matrix Temp_;
 };
+
+template<class T>
+using GRU = FastGRU<T>;
