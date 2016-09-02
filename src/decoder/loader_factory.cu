5e2bd4657085274febf3fff52d0e01eaefb5ff60
Hieu Hoang <hieuhoang@gmail.com>
2016-09-02 20:45:30 +0100

    move implementation int .cu

---
+++ src/decoder/loader_factory.cu
@@ -0,0 +1,29 @@
+#include "loader_factory.h"
+#include "scorer.h"
+#include "encoder_decoder.h"
+#include "ape_penalty.h"
+
+#ifdef KENLM
+#include "language_model.h"
+#endif
+
+LoaderPtr LoaderFactory::Create(const std::string& name,
+						const YAML::Node& config) {
+  UTIL_THROW_IF2(!config["type"],
+				 "Missing scorer type in config file");
+
+  auto type = config["type"].as<std::string>();
+  IF_MATCH_RETURN(type, "Nematus", EncoderDecoderLoader);
+  IF_MATCH_RETURN(type, "nematus", EncoderDecoderLoader);
+  IF_MATCH_RETURN(type, "NEMATUS", EncoderDecoderLoader);
+
+  IF_MATCH_RETURN(type, "Ape", ApePenaltyLoader);
+  IF_MATCH_RETURN(type, "ape", ApePenaltyLoader);
+  IF_MATCH_RETURN(type, "APE", ApePenaltyLoader);
+#ifdef KENLM
+  IF_MATCH_RETURN(type, "KenLM", KenLMLoader)
+  IF_MATCH_RETURN(type, "kenlm", KenLMLoader)
+  IF_MATCH_RETURN(type, "KENLM", KenLMLoader)
+#endif
+  UTIL_THROW2("Unknown scorer in config file: " << type);
+}
--- src/decoder/loader_factory.h
+++ src/decoder/loader_factory.h
@@ -4,13 +4,7 @@
 #include <yaml-cpp/yaml.h>
 
 #include "exception.h"
-#include "scorer.h"
-#include "encoder_decoder.h"
-#include "ape_penalty.h"
-
-#ifdef KENLM
-#include "language_model.h"
-#endif
+#include "loader.h"
 
 #define IF_MATCH_RETURN(typeVar, typeStr, LoaderType) \
 do { \
@@ -24,24 +18,6 @@ do { \
 class LoaderFactory {
   public:
     static LoaderPtr Create(const std::string& name,
-                            const YAML::Node& config) {
-      UTIL_THROW_IF2(!config["type"],
-                     "Missing scorer type in config file");
-      
-      auto type = config["type"].as<std::string>();
-      IF_MATCH_RETURN(type, "Nematus", EncoderDecoderLoader);
-      IF_MATCH_RETURN(type, "nematus", EncoderDecoderLoader);
-      IF_MATCH_RETURN(type, "NEMATUS", EncoderDecoderLoader);
-      
-      IF_MATCH_RETURN(type, "Ape", ApePenaltyLoader);
-      IF_MATCH_RETURN(type, "ape", ApePenaltyLoader);
-      IF_MATCH_RETURN(type, "APE", ApePenaltyLoader);
-#ifdef KENLM
-      IF_MATCH_RETURN(type, "KenLM", KenLMLoader)
-      IF_MATCH_RETURN(type, "kenlm", KenLMLoader)
-      IF_MATCH_RETURN(type, "KENLM", KenLMLoader)
-#endif
-      UTIL_THROW2("Unknown scorer in config file: " << type);
-    }
+                            const YAML::Node& config);
 };
 
