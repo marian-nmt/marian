4c0188d67c6ed6ecd6bc7253d54c0338608e4b1e
Hieu Hoang <hieuhoang@gmail.com>
2016-09-02 13:04:08 +0100

    abstract method Resize

--- src/mblas/base_matrix.h
+++ src/mblas/base_matrix.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <vector>
 
 namespace mblas {
+
+class BaseMatrix;
+typedef std::vector<BaseMatrix*> BaseMatrices;
 
 class BaseMatrix {
   public:
@@ -8,6 +12,9 @@ class BaseMatrix {
     
     virtual size_t Rows() const = 0;
     virtual size_t Cols() const = 0;
+    virtual void Resize(size_t rows, size_t cols) = 0;
+
 };
 
 }
+
