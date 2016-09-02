5baee8ba1a3e5ae8670edaf926ddaea5472333fe
Hieu Hoang <hieuhoang@gmail.com>
2016-09-02 22:21:15 +0100

    move BestHyps() to matrix.h

--- src/mblas/base_matrix.h
+++ src/mblas/base_matrix.h
@@ -6,6 +6,9 @@ class Hypothesis;
 typedef std::shared_ptr<Hypothesis> HypothesisPtr;
 typedef std::vector<HypothesisPtr> Beam;
 
+class Scorer;
+typedef std::shared_ptr<Scorer> ScorerPtr;
+
 namespace mblas {
 
 class BaseMatrix;
@@ -19,9 +22,11 @@ public:
 	virtual size_t Cols() const = 0;
 	virtual void Resize(size_t rows, size_t cols) = 0;
 
-    virtual void BestHyps(Beam& bestHyps, const Beam& prevHyps,
+    virtual void BestHyps(Beam& bestHyps,
+    		const Beam& prevHyps,
     		mblas::BaseMatrices& ProbsEnsemble,
-    		const size_t beamSize) const = 0;
+    		const size_t beamSize,
+    		const std::vector<ScorerPtr> &scorers) const = 0;
 
 };
 
--- src/mblas/matrix.h
+++ src/mblas/matrix.h
@@ -21,6 +21,8 @@
   
 
 #include "thrust_functions.h"
+#include "decoder/god.h"
+#include "common/types-gpu.h"
 
 namespace lib = thrust;
 namespace iterlib = thrust;
@@ -138,10 +140,110 @@ class TMatrix : public BaseMatrix {
       return data_.size();
     }
 
-    virtual void BestHyps(Beam& bestHyps, const Beam& prevHyps,
+    virtual void BestHyps(Beam& bestHyps,
+    		const Beam& prevHyps,
     		mblas::BaseMatrices& ProbsEnsemble,
-    		const size_t beamSize) const
+    		const size_t beamSize,
+    		const std::vector<ScorerPtr> &scorers) const
     {
+	  using namespace mblas;
+	  typedef TMatrix<VecType> M;
+
+	  auto& weights = God::GetScorerWeights();
+
+	  M& Probs = static_cast<M&>(*ProbsEnsemble[0]);
+
+	  M Costs(Probs.Rows(), 1);
+	  HostVector<float> vCosts;
+	  for(auto& h : prevHyps)
+		vCosts.push_back(h->GetCost());
+	  algo::copy(vCosts.begin(), vCosts.end(), Costs.begin());
+
+	  BroadcastVecColumn(weights[scorers[0]->GetName()] * _1 + _2,
+						 Probs, Costs);
+	  for(size_t i = 1; i < ProbsEnsemble.size(); ++i) {
+		  M &currProbs = static_cast<M&>(*ProbsEnsemble[i]);
+
+		  Element(_1 + weights[scorers[i]->GetName()] * _2,
+				Probs, currProbs);
+	  }
+
+	  DeviceVector<unsigned> keys(Probs.size());
+	  HostVector<unsigned> bestKeys(beamSize);
+	  HostVector<float> bestCosts(beamSize);
+
+	  // @TODO: make this more efficient
+	  if(!God::Get<bool>("allow-unk")) {
+		for(size_t i = 0; i < Probs.Rows(); i++)
+		  Probs.Set(i, UNK, std::numeric_limits<float>::lowest());
+	  }
+
+	  // @TODO: Here we need to have a partial sort
+	  if(beamSize < 10) {
+		for(size_t i = 0; i < beamSize; ++i) {
+		  DeviceVector<float>::iterator iter =
+			algo::max_element(Probs.begin(), Probs.end());
+		  bestKeys[i] = iter - Probs.begin();
+		  bestCosts[i] = *iter;
+		  *iter = std::numeric_limits<float>::lowest();
+		}
+		algo::copy(bestKeys.begin(), bestKeys.end(), keys.begin());
+	  }
+	  else {
+		// these two function do not have equivalents in
+		// in the standard library or boost, keeping thrust
+		// namespace for now
+		thrust::sequence(keys.begin(), keys.end());
+		thrust::sort_by_key(Probs.begin(), Probs.end(),
+							keys.begin(), algo::greater<float>());
+
+		algo::copy_n(keys.begin(), beamSize, bestKeys.begin());
+		algo::copy_n(Probs.begin(), beamSize, bestCosts.begin());
+	  }
+
+	  std::vector<HostVector<float>> breakDowns;
+	  bool doBreakdown = God::Get<bool>("n-best");
+	  if(doBreakdown) {
+		breakDowns.push_back(bestCosts);
+		for(size_t i = 1; i < ProbsEnsemble.size(); ++i) {
+		  HostVector<float> modelCosts(beamSize);
+		  M &currProbs = static_cast<M&>(*ProbsEnsemble[i]);
+
+		  auto it = iteralgo::make_permutation_iterator(currProbs.begin(), keys.begin());
+		  algo::copy(it, it + beamSize, modelCosts.begin());
+		  breakDowns.push_back(modelCosts);
+		}
+	  }
+
+	  for(size_t i = 0; i < beamSize; i++) {
+		size_t wordIndex = bestKeys[i] % Probs.Cols();
+		size_t hypIndex  = bestKeys[i] / Probs.Cols();
+		float cost = bestCosts[i];
+
+		HypothesisPtr hyp(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost));
+
+		if(doBreakdown) {
+		  hyp->GetCostBreakdown().resize(ProbsEnsemble.size());
+		  float sum = 0;
+		  for(size_t j = 0; j < ProbsEnsemble.size(); ++j) {
+			if(j == 0)
+			  hyp->GetCostBreakdown()[0] = breakDowns[0][i];
+			else {
+			  float cost = 0;
+			  if(j < ProbsEnsemble.size()) {
+				if(prevHyps[hypIndex]->GetCostBreakdown().size() < ProbsEnsemble.size())
+				  const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown().resize(ProbsEnsemble.size(), 0.0);
+				cost = breakDowns[j][i] + const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown()[j];
+			  }
+			  sum += weights[scorers[j]->GetName()] * cost;
+			  hyp->GetCostBreakdown()[j] = cost;
+			}
+		  }
+		  hyp->GetCostBreakdown()[0] -= sum;
+		  hyp->GetCostBreakdown()[0] /= weights[scorers[0]->GetName()];
+		}
+		bestHyps.push_back(hyp);
+	  }
 
     }
 
