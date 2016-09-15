499faceb8ea5e7b9bf4cbe9d143173961dcea3dc
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-09-15 12:23:31 +0200

    tidied up initializers

--- src/param_initializers.h
+++ src/param_initializers.h
@@ -10,25 +10,42 @@
 namespace marian {
 
 void zeros(Tensor t) {
-  std::vector<float> vals(t.size(), 0.0f);
-  thrust::copy(vals.begin(), vals.end(), t.begin());
+  t.set(0.f);
 }
 
 void ones(Tensor t) {
-  std::vector<float> vals(t.size(), 1.0f);
-  thrust::copy(vals.begin(), vals.end(), t.begin());
+  t.set(1.0f);
 }
 
-void randreal(Tensor t) {
+template <class Distribution>
+void distribution(Tensor t, float a=0.0, float b=0.1) {
   std::random_device device;
   std::default_random_engine engine(device());
-  std::uniform_real_distribution<> dist(0, 0.1);
+  Distribution dist(a, b);
   auto gen = std::bind(dist, engine);
 
   std::vector<float> vals(t.size());
   std::generate(begin(vals), end(vals), gen);
 
-  thrust::copy(vals.begin(), vals.end(), t.begin());
+  t << vals;
 }
 
+std::function<void(Tensor)> normal(float mean = 0.0, float std = 0.1) {
+  return [mean, std](Tensor t) {
+    distribution<std::normal_distribution<float>>(t, mean, std);
+  }; 
+}
+
+std::function<void(Tensor)> uniform(float a = 0.0, float b = 0.1) {
+  return [a, b](Tensor t) {
+    distribution<std::uniform_real_distribution<float>>(t, a, b);
+  };  
+}
+
+std::function<void(Tensor)> from_vector(const std::vector<float>& v) {
+  return [&v](Tensor t) {
+    t << v;
+  };
+}
+  
 } // namespace marian
