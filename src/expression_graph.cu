32a5985500b4d1101c98c854a46b340ba4249ddd
Hieu Hoang <hieuhoang@gmail.com>
2016-09-16 16:03:42 +0200

    move temp() to test.cu

--- src/expression_graph.cu
+++ src/expression_graph.cu
@@ -39,34 +39,11 @@ std::string Expr::Debug() const
 }
 
 ///////////////////////////////////////////////////////
-#include "tensor_operators.h"
-
-void temp()
-{
-	std::vector<float> hVec({1,2,  4,3,  7,9,  7,3});
-	thrust::device_vector<float> dVec(8);
-	thrust::copy(hVec.begin(), hVec.end(), dVec.begin());
-	float *data = thrust::raw_pointer_cast(dVec.data());
-
-	gSoftMax<<<4, 2, sizeof(float)>>>(data, 4, 2);
-
-	std::vector<float> hVec2(8);
-	thrust::copy(dVec.begin(), dVec.end(), hVec2.begin());
-	cerr << "hVec2=";
-	for (size_t i = 0; i < hVec.size(); ++i) {
-		cerr << hVec2[i] << " ";
-	}
-	cerr << endl;
-}
-
 ExpressionGraph::ExpressionGraph(int cudaDevice)
 : stack_(new ChainableStack)
 {
   std::srand (time(NULL));
   cudaSetDevice(0);
-
-  temp();
-  exit(0);
 }
 
 }
