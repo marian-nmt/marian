19588cbd447c334ba9e0f9c7f84906f8640ff1e5
Tapta Serverloftbig administrator <smtadmin@loft10063.serverprofi24.eu>
2016-09-27 16:50:54 +0200

    only include thurst if CUDA found

--- src/gpu/types-gpu.h
+++ src/gpu/types-gpu.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#ifndef NO_CUDA
+
 #include <thrust/device_vector.h>
 #include <thrust/functional.h>
 #include <thrust/sort.h>
@@ -14,5 +16,20 @@ using HostVector = thrust::host_vector<T>;
 
 namespace algo = thrust;
 namespace iteralgo = thrust;
+#else
 
+#include <vector>
+#include <algorithm>
+
+template<class T>
+using DeviceVector = std::vector<T>;
+
+template<class T>
+using HostVector = std::vector<T>;
+
+namespace algo = std;
+namespace iteralgo = std;
+
+
+#endif
 
