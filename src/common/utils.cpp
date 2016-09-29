9922db26dfcd06306b91453eb015798335dbe7a5
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2016-09-29 13:45:41 +0100

    Add join function to utils

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.h"
 #include <iostream>
+#include <sstream>
 
 void Trim(std::string& s) {
   boost::trim_if(s, boost::is_any_of(" \t\n"));
@@ -20,4 +21,16 @@ void Split(const std::string& line, std::vector<std::string>& pieces, const std:
     token = line.substr(begin, pos-begin);
   }
   pieces.push_back(token);
+}
+
+std::string Join(const std::vector<std::string>& words, const std::string del) {
+  std::stringstream ss;
+  if (words.empty()) {
+    return "";
+  }
+  ss << words[0];
+  for (size_t i = 1; i < words.size(); ++i) {
+    ss << del << words[i];
+  }
+  return ss.str();
 }
