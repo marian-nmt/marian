bc0050ce3f85848a1cd321dba0710301f2fde593
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2016-10-26 12:56:35 +0000

    fix bug with number of blocks in nth_element.

--- src/gpu/decoder/best_hyps.h
+++ src/gpu/decoder/best_hyps.h
@@ -13,7 +13,10 @@ namespace GPU {
 class BestHyps {
   public:
     BestHyps()
-      : nthElement_(God::Get<size_t>("beam-size"), mblas::Matrix::GetStream())
+      : nthElement_(God::Get<size_t>("beam-size"), mblas::Matrix::GetStream()),
+        keys(God::Get<size_t>("beam-size")),
+        Costs(God::Get<size_t>("beam-size")),
+        weights_(God::GetScorerWeights())
     {}
 
     void DisAllowUNK(mblas::Matrix& Prob) {
@@ -23,33 +26,8 @@ class BestHyps {
     void FindBests(size_t beamSize, mblas::Matrix& Probs,
                    std::vector<float>& outCosts,
                    std::vector<unsigned>& outKeys) {
-      if (beamSize < 10) {
-        size_t nElements = Probs.Cols() * Probs.Rows();
-        nthElement_.getNBestList(Probs.data(), nElements, beamSize, outKeys, outCosts);
-        // for (size_t i = 0; i < beamSize; ++i) {
-          // DeviceVector<float>::iterator iter =
-          // thrust::max_element(thrust::cuda::par.on(mblas::Matrix::GetStream()),
-                              // Probs.begin(), Probs.end());
-          // outKeys[i] = iter - Probs.begin();
-          // outCosts[i] = *iter;
-          // *iter = std::numeric_limits<float>::lowest();
-          // // std::cerr << outKeys[i] << ": " << outCosts[i] << "\t";
-        // }
-        // // std::cerr << std::endl;
-        // algo::copy(thrust::cuda::par.on(mblas::Matrix::GetStream()),
-                  // outKeys.begin(), outKeys.end(), keys.begin());
-      } else {
-        // thrust::sequence(thrust::cuda::par.on(mblas::Matrix::GetStream()),
-                         // keys.begin(), keys.end());
-        // thrust::sort_by_key(thrust::cuda::par.on(mblas::Matrix::GetStream()),
-                            // Probs.begin(), Probs.end(),
-                            // keys.begin(), algo::greater<float>());
-
-        // thrust::copy_n(thrust::cuda::par.on(mblas::Matrix::GetStream()),
-                       // keys.begin(), beamSize, outKeys.begin());
-        // thrust::copy_n(thrust::cuda::par.on(mblas::Matrix::GetStream()),
-                       // Probs.begin(), beamSize, outCosts.begin());
-      }
+      size_t nElements = Probs.Cols() * Probs.Rows();
+      nthElement_.getNBestList(Probs.data(), nElements, beamSize, outKeys, outCosts);
     }
 
     std::vector<SoftAlignmentPtr> GetAlignments(const std::vector<ScorerPtr>& scorers,
@@ -78,32 +56,27 @@ class BestHyps {
           bool returnAlignment) {
       using namespace mblas;
 
-      auto& weights = God::GetScorerWeights();
-
       mblas::Matrix& Probs = static_cast<mblas::Matrix&>(scorers[0]->GetProbs());
 
-      Costs.reserve(Probs.Rows());
       HostVector<float> vCosts;
       for (auto& h : prevHyps) {
         vCosts.push_back(h->GetCost());
       }
       thrust::copy(thrust::cuda::par.on(Matrix::GetStream()),
-                    vCosts.begin(), vCosts.end(), Costs.begin());
+                   vCosts.begin(), vCosts.end(), Costs.begin());
 
-      BroadcastVecColumn(weights[scorers[0]->GetName()] * _1 + _2,
-                        Probs, Costs);
+      BroadcastVecColumn(weights_[scorers[0]->GetName()] * _1 + _2, Probs, Costs);
+
       for (size_t i = 1; i < scorers.size(); ++i) {
         mblas::Matrix &currProbs = static_cast<mblas::Matrix&>(scorers[i]->GetProbs());
 
-        Element(_1 + weights[scorers[i]->GetName()] * _2,
-                Probs, currProbs);
+        Element(_1 + weights_[scorers[i]->GetName()] * _2, Probs, currProbs);
       }
 
       if (!God::Get<bool>("allow-unk")) {
         DisAllowUNK(Probs);
       }
 
-      keys.resize(Probs.size());
       std::vector<float> bestCosts(beamSize);
       std::vector<unsigned> bestKeys(beamSize);
 
@@ -114,13 +87,13 @@ class BestHyps {
       if (doBreakdown) {
           breakDowns.push_back(bestCosts);
           for (size_t i = 1; i < scorers.size(); ++i) {
-              HostVector<float> modelCosts(beamSize);
-              mblas::Matrix &currProbs = static_cast<mblas::Matrix&>(scorers[i]->GetProbs());
+            HostVector<float> modelCosts(beamSize);
+            mblas::Matrix &currProbs = static_cast<mblas::Matrix&>(scorers[i]->GetProbs());
 
-              auto it = iteralgo::make_permutation_iterator(currProbs.begin(), keys.begin());
-              algo::copy(thrust::cuda::par.on(Matrix::GetStream()),
-                          it, it + beamSize, modelCosts.begin());
-              breakDowns.push_back(modelCosts);
+            auto it = thrust::make_permutation_iterator(currProbs.begin(), keys.begin());
+            algo::copy(thrust::cuda::par.on(Matrix::GetStream()),
+                        it, it + beamSize, modelCosts.begin());
+            breakDowns.push_back(modelCosts);
           }
       }
 
@@ -138,7 +111,7 @@ class BestHyps {
         HypothesisPtr hyp;
         if (returnAlignment) {
           hyp.reset(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost,
-                                    GetAlignments(scorers, hypIndex)));
+                                   GetAlignments(scorers, hypIndex)));
         } else {
           hyp.reset(new Hypothesis(prevHyps[hypIndex], wordIndex, hypIndex, cost));
         }
@@ -156,22 +129,22 @@ class BestHyps {
                     const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown().resize(scorers.size(), 0.0f);
                   cost = breakDowns[j][i] + const_cast<HypothesisPtr&>(prevHyps[hypIndex])->GetCostBreakdown()[j];
               }
-              sum += weights[scorers[j]->GetName()] * cost;
+              sum += weights_[scorers[j]->GetName()] * cost;
               hyp->GetCostBreakdown()[j] = cost;
             }
           }
           hyp->GetCostBreakdown()[0] -= sum;
-          hyp->GetCostBreakdown()[0] /= weights[scorers[0]->GetName()];
+          hyp->GetCostBreakdown()[0] /= weights_[scorers[0]->GetName()];
         }
       bestHyps.push_back(hyp);
       }
     }
 
   private:
+    NthElement nthElement_;
     thrust::device_vector<unsigned> keys;
     thrust::device_vector<float> Costs;
-    NthElement nthElement_;
-
+    std::map<std::string, float>& weights_;
 };
 
 }
