3f8e6ab15ef9920954c2f07d8c668950942ff8d0
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2016-11-03 23:48:39 +0000

    Repair a bit encoder

--- src/gpu/dl4mt/encoder.cu
+++ src/gpu/dl4mt/encoder.cu
@@ -12,19 +12,21 @@ Encoder::Encoder(const Weights& model)
 
 void Encoder::GetContext(const std::vector<size_t>& words,
 				mblas::Matrix& Context) {
-  std::vector<mblas::Matrix> embeddedWords;
+  thread_local static std::vector<mblas::Matrix> embeddedWords;
 
   Context.Resize(words.size(), forwardRnn_.GetStateLength() + backwardRnn_.GetStateLength());
-  for(auto& w : words) {
-    embeddedWords.emplace_back();
-    embeddings_.Lookup(embeddedWords.back(), w);
+  for (size_t i = 0; i < words.size(); ++i) {
+    if (i >= embeddedWords.size()) {
+      embeddedWords.emplace_back();
+    }
+    embeddings_.Lookup(embeddedWords[i], words[i]);
   }
   //cerr << "embeddings_=" << embeddings_.w_.E_.Debug() << endl;
 
   forwardRnn_.GetContext(embeddedWords.cbegin(),
-						 embeddedWords.cend(),
+						 embeddedWords.cbegin() + words.size(),
 						 Context, false);
-  backwardRnn_.GetContext(embeddedWords.crbegin(),
+  backwardRnn_.GetContext(embeddedWords.crend() - words.size(),
 						  embeddedWords.crend(),
 						  Context, true);
 }
