94c61dfc78b60f162b615659103612d89d6a1486
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-11-11 01:20:47 +0100

    compile tensor test

--- src/tensor_test.cu
+++ src/tensor_test.cu
@@ -9,27 +9,17 @@ using namespace marian;
 int main() {
   TensorAllocator params = newTensorAllocator<DeviceGPU>();
 
-  auto t = params->tensor({9, 11});
-  t->set(1);
-  //std::cerr << t->debug() << std::endl;
+  std::vector<Tensor> tensors;
+  for (int i = 0; i < 20; ++i) {
+    tensors.emplace_back();
+    params->allocate(tensors.back(), {1000,1000});
+    std::cerr << tensors.back()->size() << std::endl;
+    tensors.back()->set(0, 3.14 * i);
+    std::cerr << params->capacity() << " " << params->size() << std::endl;
+  }
 
-  auto s = params->tensor({1, 11});
-  s->set(0);
-  Sum(s, t, 0, true);
-  std::cerr << s->debug() << std::endl;
-
-
-  //std::vector<Tensor> tensors;
-  //for (int i = 0; i < 20; ++i) {
-  //  Tensor t = params->tensor({1000,1000});
-  //  std::cerr << t->size() << std::endl;
-  //  t->set(0, 3.14 * i);
-  //  tensors.push_back(t);
-  //  std::cerr << params->capacity() << " " << params->size() << std::endl;
-  //}
-  //
-  //for(int i = 0; i < 20; i++)
-  //  std::cerr << tensors[i]->get(0) << std::endl;
+  for(int i = 0; i < 20; i++)
+    std::cerr << tensors[i]->get(0) << std::endl;
 
   return 0;
 }
