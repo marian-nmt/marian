c409a275e37c576f03fe1783f11a0625402e7c6f
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-11-16 11:47:36 +0000

    fixed dimensions

--- src/nematus_test.cu
+++ src/nematus_test.cu
@@ -21,8 +21,8 @@ void construct(ExpressionGraphPtr g,
                const SentBatch& srcSentenceBatch) {
   g->clear();
 
-  int dimSrcVoc = 30000;
-  int dimSrcEmb = 128;
+  int dimSrcVoc = 80000;
+  int dimSrcEmb = 512;
   int dimEncState = 1024;
   int dimBatch = 1;
 
@@ -36,44 +36,44 @@ void construct(ExpressionGraphPtr g,
     dimBatch = srcWordBatch.size();
   }
 
-  //auto buildEncoderGRU = [=](const std::string& prefix){
-  //  ParametersGRU encParams;
-  //  encParams.Uz = g->param(prefix + "_Uz", {dimEncState, dimEncState},
-  //                          init=glorot_uniform);
-  //  encParams.Ur = g->param(prefix + "_Ur", {dimEncState, dimEncState},
-  //                          init=glorot_uniform);
-  //
-  //  encParams.Wz = g->param(prefix + "_Wz", {dimSrcEmb, dimEncState},
-  //                          init=glorot_uniform);
-  //  encParams.Wr = g->param(prefix + "_Wr", {dimSrcEmb, dimEncState},
-  //                          init=glorot_uniform);
-  //
-  //  encParams.bz = g->param(prefix + "_bz", {1, dimEncState}, init=zeros);
-  //  encParams.br = g->param(prefix + "_br", {1, dimEncState}, init=zeros);
-  //
-  //  encParams.Ux = g->param(prefix + "_Ux", {dimEncState, dimEncState},
-  //                          init=glorot_uniform);
-  //  encParams.Wx = g->param(prefix + "_Wx", {dimSrcEmb, dimEncState},
-  //                          init=glorot_uniform);
-  //  encParams.bx = g->param(prefix + "_bx", {1, dimEncState}, init=zeros);
-  //
-  //  return RNN<GRU>(encParams);
-  //};
-
-  //auto buildEncoderGRU = [=](const std::string& prefix){
-  //  ParametersGRUFast encParams;
-  //  encParams.U = g->param(prefix + "_U", {dimEncState, 3 * dimEncState},
-  //                         init=glorot_uniform);
-  //
-  //  encParams.W = g->param(prefix + "_W", {dimSrcEmb, 3 * dimEncState},
-  //                         init=glorot_uniform);
-  //
-  //  encParams.b = g->param(prefix + "_b", {1, 3 * dimEncState}, init=zeros);
-  //
-  //  return RNN<GRUFast>(encParams);
-  //};
-
   auto buildEncoderGRU = [=](const std::string& prefix){
+    ParametersGRU encParams;
+    encParams.Uz = g->param(prefix + "_Uz", {dimEncState, dimEncState},
+                            init=glorot_uniform);
+    encParams.Ur = g->param(prefix + "_Ur", {dimEncState, dimEncState},
+                            init=glorot_uniform);
+  
+    encParams.Wz = g->param(prefix + "_Wz", {dimSrcEmb, dimEncState},
+                            init=glorot_uniform);
+    encParams.Wr = g->param(prefix + "_Wr", {dimSrcEmb, dimEncState},
+                            init=glorot_uniform);
+  
+    encParams.bz = g->param(prefix + "_bz", {1, dimEncState}, init=zeros);
+    encParams.br = g->param(prefix + "_br", {1, dimEncState}, init=zeros);
+  
+    encParams.Ux = g->param(prefix + "_Ux", {dimEncState, dimEncState},
+                            init=glorot_uniform);
+    encParams.Wx = g->param(prefix + "_Wx", {dimSrcEmb, dimEncState},
+                            init=glorot_uniform);
+    encParams.bx = g->param(prefix + "_bx", {1, dimEncState}, init=zeros);
+  
+    return RNN<GRU>(encParams);
+  };
+
+  auto buildEncoderGRU2 = [=](const std::string& prefix){
+    ParametersGRUFast encParams;
+    encParams.U = g->param(prefix + "_U", {dimEncState, 3 * dimEncState},
+                           init=glorot_uniform);
+  
+    encParams.W = g->param(prefix + "_W", {dimSrcEmb, 3 * dimEncState},
+                           init=glorot_uniform);
+  
+    encParams.b = g->param(prefix + "_b", {1, 3 * dimEncState}, init=zeros);
+  
+    return RNN<GRUFast>(encParams);
+  };
+
+  auto buildEncoderGRU3 = [=](const std::string& prefix){
     auto U = g->param(prefix + "_U", {dimEncState, 2 * dimEncState},
                       init=glorot_uniform);
 
@@ -100,11 +100,11 @@ void construct(ExpressionGraphPtr g,
 
   auto encStartState = name(g->zeros(shape={dimBatch, dimEncState}), "start");
 
-  auto encForward = buildEncoderGRU("encoder");
+  auto encForward = buildEncoderGRU3("encoder");
   auto statesForward = encForward.apply(inputs.begin(), inputs.end(),
                                         encStartState);
 
-  auto encBackward = buildEncoderGRU("encoder_r");
+  auto encBackward = buildEncoderGRU3("encoder_r");
   auto statesBackward = encBackward.apply(inputs.rbegin(), inputs.rend(),
                                           encStartState);
 
@@ -123,7 +123,7 @@ void construct(ExpressionGraphPtr g,
 }
 
 SentBatch generateBatch(size_t batchSize) {
-  size_t length = 5; // rand() % 40 + 10;
+  size_t length = rand() % 40 + 10;
   return SentBatch(length, WordBatch(batchSize));
 }
 
