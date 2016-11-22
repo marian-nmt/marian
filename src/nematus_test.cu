6cde6974cda10a42707feecd87a1df13e08f3e4e
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-11-22 12:14:38 +0100

    first decoder layer

--- src/nematus_test.cu
+++ src/nematus_test.cu
@@ -20,43 +20,62 @@ typedef DeviceVector<size_t> WordBatch;
 typedef std::vector<WordBatch> SentBatch;
 
 void load(ExpressionGraphPtr g, const std::string& name) {
-  auto model = cnpy::npz_load(name);
+  auto numpy = cnpy::npz_load(name);
 
-  auto chosen = {
+  auto parameters = {
+    // Source word embeddings
     "Wemb",
 
+    // GRU in encoder
     "encoder_U", "encoder_W", "encoder_b",
     "encoder_Ux", "encoder_Wx", "encoder_bx",
 
+    // GRU in encoder, reversed
     "encoder_r_U", "encoder_r_W", "encoder_r_b",
-    "encoder_r_Ux", "encoder_r_Wx", "encoder_r_bx"
+    "encoder_r_Ux", "encoder_r_Wx", "encoder_r_bx",
+
+    // Transformation of decoder input state
+    "ff_state_W", "ff_state_b",
+
+    // Target word embeddings
+    "Wemb_dec",
+
+    // GRU layer 1 in decoder
+    "decoder_U", "decoder_W", "decoder_b",
+    "decoder_Ux", "decoder_Wx", "decoder_bx"
   };
 
-  for(auto name : chosen) {
+  for(auto name : parameters) {
     Shape shape;
-
-    if(model[name].shape.size() == 2) {
-      shape[0] = model[name].shape[0];
-      shape[1] = model[name].shape[1];
+    if(numpy[name].shape.size() == 2) {
+      shape[0] = numpy[name].shape[0];
+      shape[1] = numpy[name].shape[1];
     }
-    else if(model[name].shape.size() == 1) {
+    else if(numpy[name].shape.size() == 1) {
       shape[0] = 1;
-      shape[1] = model[name].shape[0];
+      shape[1] = numpy[name].shape[0];
     }
 
-    auto p = g->param(name, shape, init=from_numpy(model[name]));
+    g->param(name, shape, init=from_numpy(numpy[name]));
   }
 }
 
 void construct(ExpressionGraphPtr g,
-               const SentBatch& srcSentenceBatch) {
+               const SentBatch& srcSentenceBatch,
+               const SentBatch& trgSentenceBatch) {
   g->clear();
 
   int dimSrcVoc = g->get("Wemb") ? g->get("Wemb")->shape()[0] : 85000;
   int dimSrcEmb = g->get("Wemb") ? g->get("Wemb")->shape()[1] : 500;
   int dimEncState = g->get("encoder_U") ? g->get("encoder_U")->shape()[0] : 1024;
 
+  int dimTrgVoc = g->get("Wemb_dec") ? g->get("Wemb_dec")->shape()[0] : 85000;
+  int dimTrgEmb = g->get("Wemb_dec") ? g->get("Wemb_dec")->shape()[1] : 500;
+  int dimDecState = g->get("decoder_U") ? g->get("decoder_U")->shape()[0] : 1024;
+
   int dimBatch = 1;
+
+  /****************************** encoder *********************************/
 
   auto Wemb = g->param("Wemb", {dimSrcVoc, dimSrcEmb}, init=glorot_uniform);
 
@@ -68,7 +87,7 @@ void construct(ExpressionGraphPtr g,
     dimBatch = srcWordBatch.size();
   }
 
-  auto buildEncoderGRU = [=](const std::string& prefix){
+  auto encoderGRU = [=](const std::string& prefix) {
     auto U = g->param(prefix + "_U", {dimEncState, 2 * dimEncState},
                       init=glorot_uniform);
 
@@ -93,41 +112,108 @@ void construct(ExpressionGraphPtr g,
     return RNN<GRUFast>(encParams);
   };
 
-  auto encStartState = name(g->zeros(shape={dimBatch, dimEncState}), "start");
+  auto encState0 = name(g->zeros(shape={dimBatch, dimEncState}), "start");
 
-  auto encForward = buildEncoderGRU("encoder");
-  auto statesForward = encForward.apply(inputs.begin(), inputs.end(),
-                                        encStartState);
+  auto statesFw = encoderGRU("encoder").apply(inputs.begin(),
+                                              inputs.end(),
+                                              encState0);
 
-  auto encBackward = buildEncoderGRU("encoder_r");
-  auto statesBackward = encBackward.apply(inputs.rbegin(), inputs.rend(),
-                                          encStartState);
+  auto statesBw = encoderGRU("encoder_r").apply(inputs.rbegin(),
+                                                inputs.rend(),
+                                                encState0);
 
-  std::vector<Expr> joinedStates;
-  auto itFw = statesForward.begin();
-  auto itBw = statesBackward.rbegin();
-  while(itFw != statesForward.end()) {
-    // add proper axes
-    joinedStates.push_back(concatenate({*itFw++, *itBw++}, axis=1));
-  }
+  std::vector<Expr> biStates;
+  auto itFw = statesFw.begin();
+  auto itBw = statesBw.rbegin();
+  while(itFw != statesFw.end())
+    biStates.push_back(concatenate({*itFw++, *itBw++}, axis=1));
 
   // add proper axes and make this a 3D tensor
-  auto encContext = debug(concatenate(joinedStates, axis=2), "context");
-  auto decStartState = debug(mean(encContext, axis=2), "decoderStart");
+  auto encContext = concatenate(biStates, axis=2);
+  auto meanContext = mean(encContext, axis=2);
+
+  /*** decoder layer 1 ****************************************************/
+
+  auto Wi = g->param("ff_state_W", {2 * dimEncState, dimDecState}, init=glorot_uniform);
+  auto bi = g->param("ff_state_b", {1, dimDecState}, init=zeros);
+
+  auto decState0 = debug(tanh(dot(meanContext, Wi) + bi), "decState0");
+
+  auto Wemb_dec = g->param("Wemb_dec", {dimTrgVoc, dimTrgEmb}, init=glorot_uniform);
+
+  std::vector<Expr> outputs;
+  auto emptyEmbedding = name(g->zeros(shape={dimBatch, dimTrgEmb}), "emptyEmbedding");
+  outputs.push_back(emptyEmbedding);
+
+  i = 0;
+  for(auto& trgWordBatch : srcSentenceBatch) {
+    auto y = name(rows(Wemb_dec, trgWordBatch), "y_" + std::to_string(i++));
+    outputs.push_back(y);
+  }
+
+  auto decoderGRULayer1 = [=]() {
+    auto U = g->param("decoder_U", {dimDecState, 2 * dimDecState},
+                      init=glorot_uniform);
+
+    auto W = g->param("decoder_W", {dimTrgEmb, 2 * dimDecState},
+                      init=glorot_uniform);
+
+    auto b = g->param("decoder_b", {1, 2 * dimDecState}, init=zeros);
+
+    auto Ux = g->param("decoder_Ux", {dimDecState, dimDecState},
+                      init=glorot_uniform);
+
+    auto Wx = g->param("decoder_Wx", {dimTrgEmb, dimDecState},
+                       init=glorot_uniform);
+
+    auto bx = g->param("decoder_bx", {1, dimDecState}, init=zeros);
+
+    ParametersGRUFast encParams;
+    encParams.U = concatenate({U, Ux}, axis=1);
+    encParams.W = concatenate({W, Wx}, axis=1);
+    encParams.b = concatenate({b, bx}, axis=1);
+
+    return RNN<GRUFast>(encParams);
+  };
+
+  auto statesLayer1 = decoderGRULayer1().apply(outputs.begin(),
+                                               outputs.end(),
+                                               decState0);
+
+  i = 0;
+  for(auto s : statesLayer1)
+    debug(s, "decoder state layer1 : " + std::to_string(i++));
+
+  /*** decoder layer 2 ****************************************************/
 
 }
 
-SentBatch generateBatch(size_t batchSize) {
+SentBatch generateSrcBatch(size_t batchSize) {
   //size_t length = rand() % 40 + 10;
   //return SentBatch(length, WordBatch(batchSize));
 
-  // das ist ein test . </s>
+  // das ist ein Test . </s>
   return SentBatch({
     WordBatch(batchSize, 13),
     WordBatch(batchSize, 15),
     WordBatch(batchSize, 20),
-    WordBatch(batchSize, 8306),
+    WordBatch(batchSize, 2548),
     WordBatch(batchSize, 4),
+    WordBatch(batchSize, 0)
+  });
+}
+
+SentBatch generateTrgBatch(size_t batchSize) {
+  //size_t length = rand() % 40 + 10;
+  //return SentBatch(length, WordBatch(batchSize));
+
+  // this is a test . </s>
+  return SentBatch({
+    WordBatch(batchSize, 21),
+    WordBatch(batchSize, 11),
+    WordBatch(batchSize, 10),
+    WordBatch(batchSize, 1078),
+    WordBatch(batchSize, 5),
     WordBatch(batchSize, 0)
   });
 }
@@ -145,8 +231,9 @@ int main(int argc, char** argv) {
     g->clear();
 
     // fake batch
-    auto batch = generateBatch(batchSize);
-    construct(g, batch);
+    auto srcBatch = generateSrcBatch(batchSize);
+    auto trgBatch = generateTrgBatch(batchSize);
+    construct(g, srcBatch, trgBatch);
 
     g->forward();
     g->graphviz("nematus.dot");
