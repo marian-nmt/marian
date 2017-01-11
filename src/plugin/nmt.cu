798f68b98c5590adc138cdf37688bf93904cd86a
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-01-11 11:50:34 +0000

    Add a new version of nmt plugin; start working on neuralPT

--- src/plugin/nmt.cu
+++ src/plugin/nmt.cu
@@ -2,37 +2,39 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
+#include <cmath>
 #include <boost/timer/timer.hpp>
 
 #include "nmt.h"
-#include "mblas/matrix.h"
-#include "dl4mt.h"
 #include "common/vocab.h"
-#include "common/states.h"
+#include "common/god.h"
+#include "common/history.h"
 
+using namespace GPU;
 
-using namespace mblas;
-
-NMT::NMT(const boost::shared_ptr<Weights> model,
-         const boost::shared_ptr<Vocab> src,
-         const boost::shared_ptr<Vocab> trg)
-  : debug_(false), w_(model), src_(src), trg_(trg),
-    encoder_(new Encoder(*w_)), decoder_(new Decoder(*w_)),
-    states_(new States()), firstWord_(true)
-  {
-    for(size_t i = 0; i < trg_->size(); ++i)
-      filteredId_.push_back(i);
-  }
-  
-void NMT::PrintState(StateInfoPtr ptr) {
-  std::cerr << *ptr << std::endl;
+void MosesPlugin::initGod(const std::string& configPath) {
+  std::string configs = "-c " + configPath;
+  God::Init(configs);
 }
 
-size_t NMT::GetDevices(size_t maxDevices) {
-  int num_gpus = 0;   // number of CUDA GPUs
+MosesPlugin::MosesPlugin(const std::string& configPath)
+  : debug_(false),
+    states_(new States()),
+    firstWord_(true),
+    scorers_(God::GetScorers(1)),
+    bestHyps_(God::GetBestHyps(1))
+{}
+
+/* void MosesPlugin::PrintState(StateInfoPtr ptr) { */
+  /* std::cerr << *ptr << std::endl; */
+/* } */
+
+size_t MosesPlugin::GetDevices(size_t maxDevices) {
+  int num_gpus = 0; // number of CUDA GPUs
   cudaGetDeviceCount(&num_gpus);
   std::cerr << "Number of CUDA devices: " << num_gpus << std::endl;
-  
+
   for (int i = 0; i < num_gpus; i++) {
       cudaDeviceProp dprop;
       cudaGetDeviceProperties(&dprop, i);
@@ -41,219 +43,332 @@ size_t NMT::GetDevices(size_t maxDevices) {
   return (size_t)std::min(num_gpus, (int)maxDevices);
 }
 
-void NMT::SetDevice() {
-  cudaSetDevice(w_->GetDevice());
-  CublasHandler::StaticHandle();
-}
+void MosesPlugin::GeneratePhrases(const States& states, std::string& lastWord, size_t numPhrases,
+                                  std::vector<NeuralPhrase>& phrases) {
+  Histories histories(sentences_);
 
+  size_t batchSize = 1;
+  std::vector<size_t> beamSizes(batchSize, 1);
 
-size_t NMT::GetDevice() {
-  return w_->GetDevice();
-}
-
-void NMT::ClearStates() { 
-  firstWord_ = true;
-  states_->Clear();
-}
-
-boost::shared_ptr<Weights> NMT::NewModel(const std::string& path, size_t device) {
-  std::cerr << "Got device " << device << std::endl;
-  cudaSetDevice(device);
-  CublasHandler::StaticHandle();
-  boost::shared_ptr<Weights> weights(new Weights(path, device));
-  return weights;
-}
-
-boost::shared_ptr<Vocab> NMT::NewVocab(const std::string& path) {
-  boost::shared_ptr<Vocab> vocab(new Vocab(path));
-  return vocab;
-}
-
-size_t NMT::TargetVocab(const std::string& str) {
-  return (*trg_)[str];
-}
-
-void NMT::CalcSourceContext(const std::vector<std::string>& s) {  
-  std::vector<size_t> words(s.size());
-  std::transform(s.begin(), s.end(), words.begin(),
-                 [&](const std::string& w) { return (*src_)[w]; });
-  words.push_back((*src_)["</s>"]);
-  
-  SourceContext_.reset(new Matrix());
-  Matrix& SC = *boost::static_pointer_cast<Matrix>(SourceContext_);
-  encoder_->GetContext(words, SC);
-}
-
-StateInfoPtr NMT::EmptyState() {
-  Matrix& SC = *boost::static_pointer_cast<Matrix>(SourceContext_);
-  Matrix Empty;
-  decoder_->EmptyState(Empty, SC, 1);
-  std::vector<StateInfoPtr> infos;
-  states_->SaveStates(infos, Empty);
-  return infos.back();
-}
-
-void NMT::FilterTargetVocab(const std::set<std::string>& filter) {
-  filteredId_.clear();
-  filteredId_.resize(trg_->size(), 1); // set all to UNK
-  
-  std::vector<size_t> numericFilter;
-  size_t k = 0;
-  for(auto& s : filter) {
-    size_t id = (*trg_)[s];
-    numericFilter.push_back(id);
-    filteredId_[id] = k;
-    k++;
-  }
-  // eol
-  numericFilter.push_back(numericFilter.size());
-  decoder_->Filter(numericFilter);
-}
-
-void NMT::BatchSteps(const Batches& batches, LastWords& lastWords,
-                     Scores& probsOut, Scores& unksOut, StateInfos& stateInfos,
-                     bool firstWord) {
-  Matrix& sourceContext = *boost::static_pointer_cast<Matrix>(SourceContext_);
-
-  Matrix prevEmbeddings;
-  Matrix nextEmbeddings;
-  Matrix prevStates;
-  Matrix probs;
-  Matrix nextStates;
-
-  if(firstWord) {
-    decoder_->EmptyEmbedding(prevEmbeddings, lastWords.size());
-  }
-  else {
-    // Not the first word
-    decoder_->Lookup(prevEmbeddings, lastWords);
+  Beam prevHyps(batchSize, HypothesisPtr(new Hypothesis()));
+  for (size_t i = 0; i < histories.size(); ++i) {
+    histories.at(i)->Add(prevHyps);
   }
 
-  states_->ConstructStates(prevStates, stateInfos);
+  States nextStates(scorers_.size());
 
-  for(auto& batch : batches) {
-    decoder_->MakeStep(nextStates, nextEmbeddings, probs,
-                       batch, prevStates, prevEmbeddings, sourceContext);
+  size_t vocabSize = scorers_[0]->GetVocabSize();
 
-    StateInfos tempStates;
-    states_->SaveStates(tempStates, nextStates);
+  size_t maxLength = 5;
 
-    for(size_t i = 0; i < batch.size(); ++i) {
-      if(batch[i] != 0) {
-        float p = probs(i, filteredId_[batch[i]]);
-        probsOut[i] += log(p);
-        stateInfos[i] = tempStates[i];
-      }
-      if(batch[i] == 1) {
-        unksOut[i]++;
+  for (size_t decoderStep = 0; decoderStep < maxLength; ++decoderStep) {
+    for (size_t i = 0; i < scorers_.size(); i++) {
+      Scorer &scorer = *scorers_[i];
+      State &state = *states[i];
+      State &nextState = *nextStates[i];
+
+      scorer.Score(state, nextState, beamSizes);
+    }
+
+    if (decoderStep == 0) {
+      for (auto& beamSize : beamSizes) {
+        beamSize = God::Get<size_t>("beam-size");
       }
     }
-    Swap(nextStates, prevStates);
-    Swap(nextEmbeddings, prevEmbeddings);
+
+    Beams beams(batchSize);
+
+    bestHyps_(beams, prevHyps, beamSizes, scorers_, filterIndices_, true);
+
+    for (size_t i = 0; i < batchSize; ++i) {
+      if (!beams[i].empty()) {
+        histories.at(i)->Add(beams[i], histories.at(i)->size() == maxLength);
+      }
+    }
+
+    Beam survivors;
+    for (size_t batchID = 0; batchID < batchSize; ++batchID) {
+      for (auto& h : beams[batchID]) {
+        if (h->GetWord() != EOS) {
+          survivors.push_back(h);
+        } else {
+          --beamSizes[batchID];
+        }
+      }
+    }
+
+    if (survivors.size() == 0) {
+      break;
+    }
+
+    for (size_t i = 0; i < scorers_.size(); i++) {
+      scorers_[i]->AssembleBeamState(*nextStates[i], survivors, *states[i]);
+    }
+
+    prevHyps.swap(survivors);
+  }
+
+  for (auto scorer : scorers_) {
+	  scorer->CleanUpAfterSentence();
+  }
+
+  const NBestList &nbl = histories.at(0)->NBest(God::Get<size_t>("beam-size"));
+
+  for (size_t i = 0; i < nbl.size(); ++i) {
+    const Result& result = nbl[i];
+    auto words = God::Postprocess(God::GetTargetVocab()(result.first));
+    auto& scores = result.second->GetCostBreakdown();
+
+    phrases.emplace_back(words, scores, 0, 1);
+  }
+}
+/* void MosesPlugin::SetDevice() { */
+  /* cudaSetDevice(w_->GetDevice()); */
+  /* CublasHandler::StaticHandle(); */
+/* } */
+
+/* size_t MosesPlugin::GetDevice() { */
+  /* return w_->GetDevice(); */
+/* } */
+
+/* void MosesPlugin::ClearStates() { */
+  /* firstWord_ = true; */
+  /* states_->Clear(); */
+/* } */
+
+/* size_t MosesPlugin::TargetVocab(const std::string& str) { */
+  /* return (*trg_)[str]; */
+/* } */
+
+void MosesPlugin::SetSource(const std::vector<std::string>& words) {
+  sentences_.push_back(boost::shared_ptr<Sentence>(new Sentence(0, words)));
+
+  for (auto& scorer : scorers_) {
+    scorer->SetSource(sentences_);
   }
 }
 
-void NMT::OnePhrase(
-  const std::vector<std::string>& phrase,
-  const std::string& lastWord,
-  bool firstWord,
-  StateInfoPtr inputState,
-  float& prob, size_t& unks,
-  StateInfoPtr& outputState) {
-  
-  Matrix& sourceContext = *boost::static_pointer_cast<Matrix>(SourceContext_);
-  
-  Matrix prevEmbeddings;
-  Matrix nextEmbeddings;
-  Matrix prevStates;
-  Matrix probs;
-  Matrix alignedSourceContext;
-  Matrix nextStates;
-    
-  if(firstWord) {
-    decoder_->EmptyEmbedding(prevEmbeddings, 1);
-  }
-  else {
-    // Not the first word
-    std::vector<size_t> ids = { (*trg_)[lastWord] };
-    decoder_->Lookup(prevEmbeddings, ids);
-  }
-    
-  std::vector<StateInfoPtr> inputStates = { inputState };
-  states_->ConstructStates(prevStates, inputStates);
-    
-  for(auto& w : phrase) {
-    size_t id = (*trg_)[w];
-    std::vector<size_t> nextIds = { id };
-    if(id == 1)
-      unks++;
-    
-    decoder_->MakeStep(nextStates, nextEmbeddings, probs,
-                       nextIds, prevStates, prevEmbeddings, sourceContext);
-    
-    float p = probs(0, filteredId_[id]);
-    prob += log(p);
-    
-    Swap(nextStates, prevStates);
-    Swap(nextEmbeddings, prevEmbeddings);
-  }
-  
-  std::vector<StateInfoPtr> outputStates;
-  states_->SaveStates(outputStates, prevStates);
-  outputState = outputStates.back();
-}
+/* StateInfoPtr MosesPlugin::EmptyState() { */
+  /* mblas::Matrix& SC = *boost::static_pointer_cast<mblas::Matrix>(SourceContext_); */
+  /* mblas::Matrix Empty; */
+  /* decoder_->EmptyState(Empty, SC, 1); */
+  /* std::vector<StateInfoPtr> infos; */
+  /* states_->SaveStates(infos, Empty); */
+  /* return infos.back(); */
+/* } */
 
-void NMT::MakeStep(
-  const std::vector<std::string>& nextWords,
-  const std::vector<std::string>& lastWords,
-  std::vector<StateInfoPtr>& inputStates,
-  std::vector<double>& logProbs,
-  std::vector<StateInfoPtr>& outputStates,
-  std::vector<bool>& unks) {
-  
-  Matrix& sourceContext = *boost::static_pointer_cast<Matrix>(SourceContext_);
-  
-  Matrix lastEmbeddings;
-  if(firstWord_) {
-    firstWord_ = false;
-    // Only empty state in state cache, so this is the first word
-    decoder_->EmptyEmbedding(lastEmbeddings, lastWords.size());
-  }
-  else {
-    // Not the first word
-    std::vector<size_t> lastIds(lastWords.size());
-    std::transform(lastWords.begin(), lastWords.end(), lastIds.begin(),
-                   [&](const std::string& w) { return (*trg_)[w]; });
-    decoder_->Lookup(lastEmbeddings, lastIds);
-  }
-  
-  Matrix nextEmbeddings;
-  std::vector<size_t> nextIds(nextWords.size());
-  std::transform(nextWords.begin(), nextWords.end(), nextIds.begin(),
-                 [&](const std::string& w) { return (*trg_)[w]; });
-  
-  Matrix prevStates;
-  states_->ConstructStates(prevStates, inputStates);
+/* void MosesPlugin::FilterTargetVocab(const std::set<std::string>& filter, size_t topN) { */
+  /* filteredId_.resize(topN); */
+  /* std::set<size_t> ids(filteredId_.begin(), filteredId_.end()); */
+  /* filteredId_.resize(trg_->size(), 1); // set all to UNK */
 
-  Matrix probs;
-  Matrix nextStates;
-  
-  decoder_->MakeStep(nextStates, nextEmbeddings, probs,
-                     nextIds, prevStates, lastEmbeddings, sourceContext);
-  
-  states_->SaveStates(outputStates, nextStates);
-  
-  for(auto id : nextIds) {
-    if(id != 1)
-      unks.push_back(true);
-    else
-      unks.push_back(false);
-  }
-  
-  for(size_t i = 0; i < nextIds.size(); ++i) {
-    float p = probs(i, filteredId_[nextIds[i]]);
-    //float p = probs(i, nextIds[i]);
-    logProbs.push_back(log(p));
-  }
-  
-}
+  /* size_t k = topN; */
+  /* for(auto& s : filter) { */
+    /* size_t id = (*trg_)[s]; */
+    /* if(ids.count(id) == 0) { */
+      /* ids.insert(id); */
+      /* filteredId_[id] = k; */
+      /* k++; */
+    /* } */
+  /* } */
+  /* // eol */
+  /* std::vector<size_t> numericFilter(ids.begin(), ids.end()); */
+  /* decoder_->Filter(numericFilter); */
+/* } */
+
+/* void MosesPlugin::BatchSteps(const Batches& batches, LastWords& lastWords, */
+                     /* Scores& probsOut, Scores& unksOut, StateInfos& stateInfos, */
+                     /* bool firstWord) { */
+  /* mblas::Matrix& sourceContext = *boost::static_pointer_cast<mblas::Matrix>(SourceContext_); */
+
+  /* mblas::Matrix prevEmbeddings; */
+  /* mblas::Matrix nextEmbeddings; */
+  /* mblas::Matrix prevStates; */
+  /* mblas::Matrix probs; */
+  /* mblas::Matrix nextStates; */
+
+  /* if(firstWord) { */
+    /* decoder_->EmptyEmbedding(prevEmbeddings, lastWords.size()); */
+  /* } */
+  /* else { */
+    /* decoder_->Lookup(prevEmbeddings, lastWords); */
+  /* } */
+
+  /* states_->ConstructStates(prevStates, stateInfos); */
+
+  /* for(auto& batch : batches) { */
+    /* decoder_->MakeStep(nextStates, probs, prevStates, prevEmbeddings, sourceContext); */
+    /* decoder_->Lookup(nextEmbeddings, batch); */
+    /* StateInfos tempStates; */
+    /* states_->SaveStates(tempStates, nextStates); */
+
+    /* for(size_t i = 0; i < batch.size(); ++i) { */
+      /* if(batch[i] != 0) { */
+        /* float p = probs(i, filteredId_[batch[i]]); */
+        /* probsOut[i] += log(p); */
+        /* stateInfos[i] = tempStates[i]; */
+      /* } */
+      /* if(batch[i] == 1) { */
+        /* unksOut[i]++; */
+      /* } */
+    /* } */
+
+    /* Swap(nextStates, prevStates); */
+    /* Swap(nextEmbeddings, prevEmbeddings); */
+  /* } */
+/* } */
+
+/* void MosesPlugin::OnePhrase( */
+  /* const std::vector<std::string>& phrase, */
+  /* const std::string& lastWord, */
+  /* bool firstWord, */
+  /* StateInfoPtr inputState, */
+  /* float& prob, size_t& unks, */
+  /* StateInfoPtr& outputState) { */
+
+  /* mblas::Matrix& sourceContext = *boost::static_pointer_cast<mblas::Matrix>(SourceContext_); */
+
+  /* mblas::Matrix prevEmbeddings; */
+  /* mblas::Matrix nextEmbeddings; */
+  /* mblas::Matrix prevStates; */
+  /* mblas::Matrix probs; */
+  /* mblas::Matrix alignedSourceContext; */
+  /* mblas::Matrix nextStates; */
+
+  /* if(firstWord) { */
+    /* decoder_->EmptyEmbedding(prevEmbeddings, 1); */
+  /* } */
+  /* else { */
+    /* // Not the first word */
+    /* std::vector<size_t> ids = { (*trg_)[lastWord] }; */
+    /* decoder_->Lookup(prevEmbeddings, ids); */
+  /* } */
+
+  /* std::vector<StateInfoPtr> inputStates = { inputState }; */
+  /* states_->ConstructStates(prevStates, inputStates); */
+
+  /* for(auto& w : phrase) { */
+    /* size_t id = (*trg_)[w]; */
+    /* std::vector<size_t> nextIds = { id }; */
+    /* if(id == 1) */
+      /* unks++; */
+
+    /* decoder_->MakeStep(nextStates, probs, */
+                       /* prevStates, prevEmbeddings, sourceContext); */
+    /* decoder_->Lookup(nextEmbeddings, nextIds); */
+    /* float p = probs(0, filteredId_[id]); */
+    /* prob += log(p); */
+
+    /* Swap(nextStates, prevStates); */
+    /* Swap(nextEmbeddings, prevEmbeddings); */
+  /* } */
+
+  /* std::vector<StateInfoPtr> outputStates; */
+  /* states_->SaveStates(outputStates, prevStates); */
+  /* outputState = outputStates.back(); */
+/* } */
+
+/* void MosesPlugin::MakeStep( */
+  /* const std::vector<std::string>& nextWords, */
+  /* const std::vector<std::string>& lastWords, */
+  /* std::vector<StateInfoPtr>& inputStates, */
+  /* std::vector<double>& logProbs, */
+  /* std::vector<StateInfoPtr>& outputStates, */
+  /* std::vector<bool>& unks) { */
+
+  /* mblas::Matrix& sourceContext = *boost::static_pointer_cast<mblas::Matrix>(SourceContext_); */
+
+  /* mblas::Matrix lastEmbeddings; */
+  /* if(firstWord_) { */
+    /* firstWord_ = false; */
+    /* // Only empty state in state cache, so this is the first word */
+    /* decoder_->EmptyEmbedding(lastEmbeddings, lastWords.size()); */
+  /* } */
+  /* else { */
+    /* // Not the first word */
+    /* std::vector<size_t> lastIds(lastWords.size()); */
+    /* std::transform(lastWords.begin(), lastWords.end(), lastIds.begin(), */
+                   /* [&](const std::string& w) { return (*trg_)[w]; }); */
+    /* decoder_->Lookup(lastEmbeddings, lastIds); */
+  /* } */
+
+  /* mblas::Matrix nextEmbeddings; */
+  /* std::vector<size_t> nextIds(nextWords.size()); */
+  /* std::transform(nextWords.begin(), nextWords.end(), nextIds.begin(), */
+                 /* [&](const std::string& w) { return (*trg_)[w]; }); */
+
+  /* mblas::Matrix prevStates; */
+  /* states_->ConstructStates(prevStates, inputStates); */
+
+  /* mblas::Matrix probs; */
+  /* mblas::Matrix nextStates; */
+
+  /* decoder_->MakeStep(nextStates, probs, */
+                     /* prevStates, lastEmbeddings, sourceContext); */
+  /* decoder_->Lookup(nextEmbeddings, nextIds); */
+  /* states_->SaveStates(outputStates, nextStates); */
+
+  /* for(auto id : nextIds) { */
+    /* if(id != 1) */
+      /* unks.push_back(true); */
+    /* else */
+      /* unks.push_back(false); */
+  /* } */
+
+  /* for(size_t i = 0; i < nextIds.size(); ++i) { */
+    /* float p = probs(i, filteredId_[nextIds[i]]); */
+    /* //float p = probs(i, nextIds[i]); */
+    /* logProbs.push_back(log(p)); */
+  /* } */
+/* } */
+
+/* std::vector<double> MosesPlugin::RescoreNBestList( */
+    /* const std::vector<std::string>& nbest, */
+    /* const size_t maxBatchSize) { */
+
+  /* mblas::Matrix PrevState; */
+  /* mblas::Matrix PrevEmbedding; */
+  /* mblas::Matrix Probs; */
+  /* mblas::Matrix State; */
+  /* mblas::Matrix Embedding; */
+
+  /* NBest nBest(src_, trg_, nbest); */
+
+  /* std::vector<double> nBestScores; */
+  /* for (auto& batch: nBest.DivideNBestListIntoBatches()) { */
+    /* size_t batchSize = batch[0].size(); */
+
+    /* decoder_->EmptyState( */
+        /* PrevState, */
+        /* *boost::static_pointer_cast<mblas::Matrix>(SourceContext_), */
+        /* batchSize); */
+    /* decoder_->EmptyEmbedding(PrevEmbedding, batchSize); */
+
+    /* std::vector<float> scores(batch[0].size(), 0.0f); */
+    /* size_t lengthIndex = 0; */
+    /* for (auto& w : batch) { */
+      /* decoder_->MakeStep(State, Probs, PrevState, PrevEmbedding, */
+          /* *boost::static_pointer_cast<mblas::Matrix>(SourceContext_)); */
+
+      /* for (size_t j = 0; j < w.size(); ++j) { */
+        /* if (batch[lengthIndex][j]) { */
+          /* float p = Probs(j, w[j]); */
+          /* scores[j] += log(p); */
+        /* } */
+      /* } */
+
+      /* decoder_->Lookup(Embedding, w); */
+
+      /* mblas::Swap(State, PrevState); */
+      /* mblas::Swap(Embedding, PrevEmbedding); */
+      /* ++lengthIndex; */
+    /* } */
+
+    /* for (int i = 0; i < scores.size(); ++i) { */
+      /* nBestScores.push_back(scores[i]); */
+    /* } */
+  /* } */
+  /* return nBestScores; */
+/* } */
+
+
