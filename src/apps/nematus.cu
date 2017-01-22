79c9e20bb1b84733c612e93161b663c45037853e
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2017-01-22 12:42:22 +0000

    added epoch counting

--- src/apps/nematus.cu
+++ src/apps/nematus.cu
@@ -206,6 +206,7 @@ int main(int argc, char** argv) {
           nematus->save(modelPrefix + "." + std::to_string(batches) + ".npz");
       }
     }
+    epochs++;
   }
   std::cout << "Training finshed" << std::endl;
   nematus->save(modelPrefix + ".npz");
