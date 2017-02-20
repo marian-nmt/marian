6ce7162c7f8c70c64e552bc4c4cf69040e5a92f9
Hieu Hoang <hieuhoang@gmail.com>
2017-02-20 10:49:40 +0000

    tweak client script

--- scripts/client.py
+++ scripts/client.py
@@ -2,8 +2,12 @@
 
 from websocket import create_connection
 import time
+import sys
 
-with open("testfile.en") as f:
+filePath = sys.argv[1]
+print filePath
+
+with open(filePath) as f:
   for line in f:
     ws = create_connection("ws://localhost:8080/translate")
     ws.send(line)
