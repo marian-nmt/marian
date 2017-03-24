73596bb556254dc44ed3747068bca9f6f1854f5f
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2017-03-24 20:48:18 +0100

    valid script

--- examples/training/scripts/validate.sh
+++ examples/training/scripts/validate.sh
@@ -9,9 +9,8 @@ ref=data/newsdev2016.tok.en
 # decode
 
 cat $dev | ../../build/amun -c $prefix.dev.npz.amun.yml -b 12 -n --mini-batch 10 --maxi-batch 100 2>/dev/null \
- | sed 's/\@\@ //g' | mosesdecoder/scripts/recaser/detruecase.perl > $dev.output.postprocessed
+ | sed 's/\@\@ //g' | ./mosesdecoder/scripts/recaser/detruecase.perl > $dev.output.postprocessed
 
 ## get BLEU
-BLEU=`./mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev.output.postprocessed | cut -f 3 -d ' ' | cut -f 1 -d ','`
+./mosesdecoder/scripts/generic/multi-bleu.perl $ref < $dev.output.postprocessed | cut -f 3 -d ' ' | cut -f 1 -d ','
 
-echo $BLEU
