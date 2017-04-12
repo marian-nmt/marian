c86fd97c0dd2cffa185a4b2bd7b9fe1fa158933b
Alham Fikri Aji <afaji321@gmail.com>
2017-04-12 15:25:23 +0000

    fix validation script error

--- examples/training/scripts/validate.sh
+++ examples/training/scripts/validate.sh
@@ -10,8 +10,8 @@ ref=$DATAPATH/newsdev2016.tok.en
 # decode
 
 cat $dev | ../../build/amun -c $prefix.dev.npz.amun.yml -b 12 -n --mini-batch 10 --maxi-batch 100 2>/dev/null \
- | sed 's/\@\@ //g' | ./moses-scripts/scripts/recaser/detruecase.perl > $dev.output.postprocessed
+ | sed 's/\@\@ //g' | ./moses-scripts/scripts/recaser/detruecase.perl > output.postprocessed
 
 ## get BLEU
-./moses-scripts/scripts/generic/multi-bleu.perl $ref < $dev.output.postprocessed | cut -f 3 -d ' ' | cut -f 1 -d ','
+./moses-scripts/scripts/generic/multi-bleu.perl $ref < output.postprocessed | cut -f 3 -d ' ' | cut -f 1 -d ','
 
