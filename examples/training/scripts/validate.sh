2f18952c738fd161b15d3aa59e54a8d066381c19
Roman Grundkiewicz <romang@amu.edu.pl>
2017-04-14 13:03:15 +0200

    Fix and clean examples

--- examples/training/scripts/validate.sh
+++ examples/training/scripts/validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#model prefix
+# model prefix
 prefix=model/model.npz
 
 dev=data/newsdev2016.bpe.ro
@@ -9,8 +9,7 @@ ref=data/newsdev2016.tok.en
 # decode
 
 cat $dev | ../../build/amun -c $prefix.dev.npz.amun.yml -b 12 -n --mini-batch 10 --maxi-batch 100 2>/dev/null \
- | sed 's/\@\@ //g' | ./moses-scripts/scripts/recaser/detruecase.perl > $dev.output.postprocessed
+    | sed 's/\@\@ //g' | ./moses-scripts/scripts/recaser/detruecase.perl > $dev.output.postprocessed
 
-## get BLEU
+# get BLEU
 ./moses-scripts/scripts/generic/multi-bleu.perl $ref < $dev.output.postprocessed | cut -f 3 -d ' ' | cut -f 1 -d ','
-
