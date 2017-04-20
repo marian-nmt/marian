bea27d2d29628ebee6b4ea58044dd03d2525b794
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-04-20 11:38:22 +0100

    Implement a new NBesstList class

--- src/plugin/nbest.cpp
+++ src/plugin/nbest.cpp
@@ -8,11 +8,54 @@
 
 namespace amunmt {
 
+RescoreBatch::RescoreBatch(std::vector<std::vector<int>>& data, std::vector<States>& states)
+  : data(data),
+    states(states)
+{
+    ComputePrevIds();
+    ComputeIndices();
+}
+
+void RescoreBatch::ComputePrevIds() {
+  std::vector<size_t> ids;
+  for (size_t i = 0; i < data[0].size() - 1; ++i) {
+    ids.push_back(i);
+  }
+  prevIds.push_back(ids);
+
+  for (size_t step = 0; step < data.size() - 1; ++step) {
+    std::vector<size_t> nextIds;
+    std::vector<size_t> compIds;
+    for (size_t i = 0; i < prevIds[step].size(); ++i) {
+      if (data[step + 1][prevIds[step][i]] != -1) {
+        nextIds.push_back(prevIds[step][i]);
+      } else {
+        compIds.push_back(prevIds[step][i]);
+      }
+    }
+    prevIds.emplace_back(std::move(nextIds));
+    completed.emplace_back(std::move(compIds));
+  }
+}
+
+void RescoreBatch::ComputeIndices() {
+  for (size_t batchIdx = 0; batchIdx < data.size(); ++batchIdx) {
+    std::vector<std::pair<size_t, size_t>> ids;
+    for (size_t i = 0; i < prevIds.size(); ++i) {
+      ids.push_back(std::make_pair(i, data[batchIdx][prevIds[batchIdx][i]]));
+    }
+    indices.emplace_back(std::move(ids));
+  }
+}
+
+
 NBest::NBest(
   const std::vector<std::string>& nBestList,
+  const States& states,
   Vocab& trgVocab,
   const size_t maxBatchSize)
-    : maxBatchSize_(maxBatchSize)
+    : states_(1, states),
+      maxBatchSize_(maxBatchSize)
 {
   for (size_t i = 0; i < nBestList.size(); ++i) {
     std::vector<std::string> tokens;
@@ -25,14 +68,38 @@ NBest::NBest(
   }
 }
 
+NBest::NBest(
+  const std::vector<std::vector<std::string>>& nBestList,
+  const std::vector<States>& states,
+  Vocab& trgVocab,
+  const size_t maxBatchSize)
+    : maxBatchSize_(maxBatchSize),
+      states_(states)
+{
+  for (auto& tokens : nBestList) {
+    std::vector<int> ids;
+    for (auto token : trgVocab(tokens, false)) {
+      ids.push_back((int)token);
+    }
+    data_.push_back(ids);
+  }
+}
 
-std::vector<NBestBatch> NBest::SplitNBestListIntoBatches() const {
-  std::vector<NBestBatch> batches;
+std::vector<RescoreBatch> NBest::SplitNBestListIntoBatches() const {
+  std::vector<RescoreBatch> batches;
   std::vector<std::vector<int>> sBatch;
   for (size_t i = 0; i < data_.size(); ++i) {
+    std::vector<States> inputStates;
     sBatch.push_back(data_[i]);
+    if (states_.size() == 1) {
+      inputStates.push_back(states_[0]);
+    } else {
+      inputStates.push_back(states_[i]);
+    }
+
     if (sBatch.size() == maxBatchSize_ || i == data_.size() - 1) {
-      batches.push_back(MaskAndTransposeBatch(sBatch));
+      MaskAndTransposeBatch(sBatch);
+      batches.emplace_back(sBatch, inputStates);
       sBatch.clear();
     }
   }
@@ -40,7 +107,7 @@ std::vector<NBestBatch> NBest::SplitNBestListIntoBatches() const {
 }
 
 
-inline NBestBatch NBest::MaskAndTransposeBatch(const NBestBatch& batch) const {
+inline void NBest::MaskAndTransposeBatch(NBestBatch& batch) const {
   size_t maxLength = 0;
   for (auto& sentence: batch) {
     maxLength = std::max(maxLength, sentence.size());
@@ -56,7 +123,7 @@ inline NBestBatch NBest::MaskAndTransposeBatch(const NBestBatch& batch) const {
       }
     }
   }
-  return masked;
+  std::swap(batch, masked);
 }
 
 }  // namespace amunmt
--- src/plugin/nbest.h
+++ src/plugin/nbest.h
@@ -4,26 +4,53 @@
 #include <vector>
 #include <memory>
 
+#include "common/scorer.h"
+
+
 
 using NBestBatch = std::vector<std::vector<int>>;
 
 namespace amunmt {
 class Vocab;
 
+class RescoreBatch {
+  public:
+    RescoreBatch(std::vector<std::vector<int>>& data, std::vector<States>& states);
+
+    std::vector<std::vector<int>> data;
+    std::vector<States> states;
+    std::vector<std::vector<std::pair<size_t, size_t>>> indices;
+    std::vector<std::vector<size_t>> prevIds;
+    std::vector<std::vector<size_t>> completed;
+
+  protected:
+    void ComputePrevIds();
+    void ComputeIndices();
+
+};
+
 class NBest {
   public:
     NBest(
       const std::vector<std::string>& nBestList,
+      const States& states,
       Vocab& trgVocab,
       const size_t maxBatchSize=64);
 
-    std::vector<NBestBatch> SplitNBestListIntoBatches() const;
+    NBest(
+      const std::vector<std::vector<std::string>>& nBestList,
+      const std::vector<States>& states,
+      Vocab& trgVocab,
+      const size_t maxBatchSize=64);
+
+    std::vector<RescoreBatch> SplitNBestListIntoBatches() const;
 
   protected:
-    NBestBatch MaskAndTransposeBatch(const NBestBatch& batch) const;
+    void MaskAndTransposeBatch(NBestBatch& batch) const;
 
   protected:
     std::vector<std::vector<int>> data_;
+    std::vector<States> states_;
     const size_t maxBatchSize_;
 };
 
