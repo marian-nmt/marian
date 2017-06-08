a8f259353d725a5207079e7cd16c018edf23dad3
Hieu Hoang <hieuhoang@gmail.com>
2017-06-08 13:00:53 -0400

    debug

--- src/amun/common/sentences.cpp
+++ src/amun/common/sentences.cpp
@@ -31,6 +31,8 @@ class LengthOrderer {
 
 void Sentences::SortByLength() {
   std::sort(coll_.rbegin(), coll_.rend(), LengthOrderer());
+  //std::sort(coll_.begin(), coll_.end(), LengthOrderer());
+  //std::random_shuffle ( coll_.begin(), coll_.end() );
 }
 
 SentencesPtr Sentences::NextMiniBatch(size_t batchsize)
