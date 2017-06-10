d525ca0b28b86e767e50ea20db902946cb9bcd9f
Hieu Hoang <hieuhoang@gmail.com>
2017-06-09 22:43:04 -0400

    debug

--- src/amun/gpu/mblas/nth_element.cu
+++ src/amun/gpu/mblas/nth_element.cu
@@ -318,7 +318,7 @@ void NthElement::getNBestList(mblas::Matrix &probs, const std::vector<int>& batc
     (outWrap, probsWrap, batchPositionWrap, resWrap, cumBeamSizesWrap,
      numBlocks);
 
-
+  /*
   cerr << "numBlocks=" << numBlocks << endl;
   cerr << "numBatches=" << numBatches << endl;
   cerr << "threads=" << BLOCK_SIZE << endl;
@@ -337,7 +337,7 @@ void NthElement::getNBestList(mblas::Matrix &probs, const std::vector<int>& batc
   //cerr << mblas::Debug(d_cumBeamSizes, 2) << endl;
 
   cerr << endl;
-
+  */
 }
 
 void NthElement::getNBestList(const std::vector<size_t>& beamSizes, mblas::Matrix& Probs,
