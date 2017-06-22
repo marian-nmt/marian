4560c0dd9f496b56217d72183ff4058fca010260
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-06-22 10:50:56 +0100

    Support S2S model in rescorer

--- src/command/s2s_rescorer.cu
+++ src/command/s2s_rescorer.cu
@@ -1,12 +1,11 @@
 #include "marian.h"
 #include "training/rescorer.h"
-#include "models/amun.h"
 
 int main(int argc, char** argv) {
   using namespace marian;
 
   auto options = New<Config>(argc, argv, true, false);
-  auto task = New<Rescorer<Amun>>(options);
+  auto task = rescorerByType(options);
 
   boost::timer::cpu_timer timer;
   task->run();
--- src/training/rescorer.h
+++ src/training/rescorer.h
@@ -5,20 +5,22 @@
 #include "data/batch_generator.h"
 #include "data/corpus.h"
 #include "graph/expression_graph.h"
+#include "models/amun.h"
 #include "models/model_task.h"
+#include "models/s2s.h"
 #include "training/config.h"
 
 namespace marian {
 
 using namespace data;
 
-template<class Model>
+template <class Model>
 class Rescorer : public ModelTask {
 private:
   Ptr<Config> options_;
 
 public:
-  Rescorer(Ptr<Config> options) : options_(options) { }
+  Rescorer(Ptr<Config> options) : options_(options) {}
 
   void run() {
     auto corpus = New<data::Corpus>(options_);
@@ -47,10 +49,21 @@ public:
       std::vector<float> scores;
       costNode->val()->get(scores);
 
-      for(auto score: scores)
+      for(auto score : scores)
         std::cout << score << std::endl;
     }
   }
 };
 
+Ptr<ModelTask> rescorerByType(Ptr<Config> options) {
+  std::string type = options->get<std::string>("type");
+
+  if(type == "s2s") {
+    return New<Rescorer<S2S>>(options);
+  } else if(type == "amun") {
+    return New<Rescorer<Amun>>(options);
+  } else {
+    UTIL_THROW2("Unknown model type: " + type);
+  }
+}
 }
