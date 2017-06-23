63215f452157ca075f89f81373fdf68dff91de3d
Hieu Hoang <hieuhoang@gmail.com>
2017-06-23 17:02:28 +0100

    begin discarding pointer arithmetic.2

--- src/amun/gpu/dl4mt/gru.cu
+++ src/amun/gpu/dl4mt/gru.cu
@@ -14,6 +14,7 @@ __global__ void gElementwiseOps(mblas::MatrixWrapper<float> outWrap,
   const uint rows = stateWrap.dim(0) * stateWrap.dim(2) * stateWrap.dim(3);
   const uint cols = stateWrap.dim(1);
   assert(blockIdx.x < rows);
+  assert(ruhWrap.dim(1) == cols * 3);
 
   const float* rowRuh = &ruhWrap[0] + blockIdx.x * cols * 3;
   const float* rowT = &tempWrap[0] + blockIdx.x * cols * 3;
@@ -24,11 +25,19 @@ __global__ void gElementwiseOps(mblas::MatrixWrapper<float> outWrap,
   for(int tid = 0; tid < cols; tid += blockDim.x) {
     int i = tid + threadIdx.x;
     if(i < cols) {
-      float ev1 = expf(-(rowRuh[i] + bWrap[i] + rowT[i]));
+      float ev1 = expf(-(rowRuh[i]
+                         + bWrap[i]
+                         + tempWrap[blockIdx.x * tempWrap.dim(1) + i]
+                        )
+                      );
       float r = 1.0f / (1.0f + ev1);
 
       int k = i + cols;
-      float ev2 = expf(-(rowRuh[k] + bWrap[k] + rowT[k]));
+      float ev2 = expf(-(rowRuh[k]
+                         + bWrap[k]
+                         + tempWrap[blockIdx.x * tempWrap.dim(1) + k]
+                        )
+                      );
       float u = 1.0f / (1.0f + ev2);
 
       float hv = rowH[i] + bx1Wrap[i];
