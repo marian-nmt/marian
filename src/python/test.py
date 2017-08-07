676613cf4f880481f36f047e5aeb476c44890a78
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-08-07 10:57:02 +0100

    Set output directory for python library

--- src/python/test.py
+++ src/python/test.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "../../build/src/python"))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "../../build"))
 
 import libmariannmt as nmt
 
