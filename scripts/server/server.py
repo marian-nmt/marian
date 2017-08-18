68363b817d3d28dea1a94c36d994c39451a95dd3
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-08-18 22:10:50 +0100

    Remove unwanted print in server.py

--- scripts/server/server.py
+++ scripts/server/server.py
@@ -6,7 +6,6 @@ import os
 import argparse
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "./../build"))
-print sys.path
 import libmariannmt as nmt
 
 from bottle import request, Bottle, abort
