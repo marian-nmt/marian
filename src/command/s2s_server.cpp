e7913aad332caa25ac02503757d4a96d916e76c9
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-08-19 09:56:26 +0100

    Refactorize server code

--- src/command/s2s_server.cpp
+++ src/command/s2s_server.cpp
@@ -6,7 +6,7 @@
 
 typedef SimpleWeb::SocketServer<SimpleWeb::WS> WsServer;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   using namespace marian;
 
   auto options = New<Config>(argc, argv, ConfigMode::translating);
@@ -15,58 +15,35 @@ int main(int argc, char** argv) {
   WsServer server_;
   server_.config.port = 1234;
 
-  auto &echo = server_.endpoint["^/translate/?$"];
+  auto &translate = server_.endpoint["^/translate/?$"];
 
-  echo.on_message = [&task](std::shared_ptr<WsServer::Connection> connection,
-                       std::shared_ptr<WsServer::Message> message) {
+  translate.on_message = [&task](Ptr<WsServer::Connection> connection,
+                                 Ptr<WsServer::Message> message) {
     auto message_str = message->string();
-
-    std::cout << "Server: Message received: \"" << message_str << "\" from "
-              << connection.get() << std::endl;
-    std::cout << "Server: Sending message \"" << message_str << "\" to "
-              << connection.get() << std::endl;
+    LOG(info)->info("Message received: \"" + message_str + "\"");
 
     auto send_stream = std::make_shared<WsServer::SendStream>();
     boost::timer::cpu_timer timer;
-    for(auto& transl : task->run({message_str})) {
+    for(auto &transl : task->run({message_str})) {
       *send_stream << transl << std::endl;
     }
     LOG(info)->info("Search took: {}", timer.format(5, "%ws"));
 
-    // connection->send is an asynchronous function
     connection->send(send_stream, [](const SimpleWeb::error_code &ec) {
       if(ec) {
-        std::cout << "Server: Error sending message. " <<
-            // Error Codes for error code meanings:
-            // http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html
-            "Error: " << ec << ", error message: " << ec.message() << std::endl;
+        auto ec_str = std::to_string(ec.value());
+        LOG(warn)
+            ->warn("Error sending message: (" + ec_str + ") " + ec.message());
       }
     });
-
-    // Alternatively, using a convenience function:
-    // connection->send(message_str, [](const SimpleWeb::error_code & /*ec*/) {
-    // /*handle error*/ });
-  };
-
-  echo.on_open = [](std::shared_ptr<WsServer::Connection> connection) {
-    std::cout << "Server: Opened connection " << connection.get() << std::endl;
-  };
-
-  // See RFC 6455 7.4.1. for status codes
-  echo.on_close = [](std::shared_ptr<WsServer::Connection> connection,
-                     int status,
-                     const std::string & /*reason*/) {
-    std::cout << "Server: Closed connection " << connection.get()
-              << " with status code " << status << std::endl;
   };
 
   // Error Codes for error code meanings
-  // http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html,
-  echo.on_error = [](std::shared_ptr<WsServer::Connection> connection,
-                     const SimpleWeb::error_code &ec) {
-    std::cout << "Server: Error in connection " << connection.get() << ". "
-              << "Error: " << ec << ", error message: " << ec.message()
-              << std::endl;
+  // http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html
+  translate.on_error = [](Ptr<WsServer::Connection> connection,
+                          const SimpleWeb::error_code &ec) {
+    auto ec_str = std::to_string(ec.value());
+    LOG(warn)->warn("Connection error: (" + ec_str + ") " + ec.message());
   };
 
   std::thread server_thread([&server_]() {
