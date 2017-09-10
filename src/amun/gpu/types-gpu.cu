90f7f1cfc47ad43ffa206e7fbc2ed153f654763e
Hieu Hoang <hieuhoang@gmail.com>
2017-09-10 20:37:51 +0200

    namespace

--- src/amun/gpu/types-gpu.cu
+++ src/amun/gpu/types-gpu.cu
@@ -1,8 +1,14 @@
 #include "types-gpu.h"
 
+namespace amunmt {
+namespace GPU {
+
 void HandleError(cudaError_t err, const char *file, int line ) {
   if (err != cudaSuccess) {
     std::cerr << "ERROR: " << cudaGetErrorString(err) << " in " << file << " at line " << line << std::endl;
     exit( EXIT_FAILURE );
   }
+}
+
+}
 }
