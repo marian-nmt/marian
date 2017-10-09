10fd58b40ba8ffe638091293072d8a1efbc5b9bd
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-09 08:49:50 +0000

    Refactorize model factories

--- src/models/model_factory.h
+++ src/models/model_factory.h
@@ -13,18 +13,6 @@
 #include "examples/mnist/model_lenet.h"
 #endif
 
-#define REGISTER_ENCODER(name, className)\
-do {\
-if(options_->get<std::string>("type") == name)\
-  return New<className>(options_);\
-} while(0)
-
-#define REGISTER_DECODER(name, className)\
-do {\
-if(options_->get<std::string>("type") == name)\
-  return New<className>(options_);\
-} while(0)
-
 #define REGISTER_ENCODER_DECODER(name, className)\
 do {\
 if(options_->get<std::string>("type") == name)\
@@ -41,10 +29,12 @@ public:
   EncoderFactory(Ptr<ExpressionGraph> graph = nullptr) : Factory(graph) {}
 
   virtual Ptr<EncoderBase> construct() {
-
-    REGISTER_ENCODER("s2s", EncoderS2S);
-    REGISTER_ENCODER("transformer", EncoderTransformer);
-    REGISTER_ENCODER("transformer_gru", EncoderTransformerGRU);
+    if(options_->get<std::string>("type") == "s2s")
+      return New<EncoderS2S>(options_);
+    if(options_->get<std::string>("type") == "transformer")
+      return New<EncoderTransformer>(options_);
+    if(options_->get<std::string>("type") == "transformer_gru")
+      return New<EncoderTransformerGRU>(options_);
 
     UTIL_THROW2("Unknown encoder type");
   }
@@ -58,9 +48,12 @@ public:
 
   virtual Ptr<DecoderBase> construct() {
 
-    REGISTER_DECODER("s2s", DecoderS2S);
-    REGISTER_DECODER("transformer", DecoderTransformer);
-    REGISTER_DECODER("transformer_gru", DecoderTransformerGRU);
+    if(options_->get<std::string>("type") == "s2s")
+      return New<DecoderS2S>(options_);
+    if(options_->get<std::string>("type") == "transformer")
+      return New<DecoderTransformer>(options_);
+    if(options_->get<std::string>("type") == "transformer_gru")
+      return New<DecoderTransformerGRU>(options_);
 
     UTIL_THROW2("Unknown decoder type");
   }
@@ -89,8 +82,10 @@ public:
   virtual Ptr<EncoderDecoder> construct() {
     Ptr<EncoderDecoder> encdec;
 
-    REGISTER_ENCODER_DECODER("amun", Amun);
-    REGISTER_ENCODER_DECODER("nematus", Nematus);
+    if(options_->get<std::string>("type") == "amun")
+      encdec = New<Amun>(options_);
+    if(options_->get<std::string>("type") == "nematus")
+      encdec = New<Nematus>(options_);
 
     if(!encdec)
       encdec = New<EncoderDecoder>(options_);
@@ -111,70 +106,50 @@ Ptr<ModelBase> by_type(std::string type,
                        Ptr<Options> options) {
 
   if(type == "s2s" || type == "amun" || type == "nematus") {
-    return models::encoder_decoder()
-           (options)
-           .push_back(models::encoder()
-                      ("type", "s2s")
-                      ("original-type", type))
-           .push_back(models::decoder()
-                      ("type", "s2s")
-                      ("original-type", type))
-           .construct();
+    return models::encoder_decoder()(options)
+        .push_back(models::encoder()("type", "s2s")("original-type", type))
+        .push_back(models::decoder()("type", "s2s")("original-type", type))
+        .construct();
   }
 
   if(type == "transformer") {
-    return models::encoder_decoder()
-           (options)
-           .push_back(models::encoder()
-                      ("type", "transformer"))
-           .push_back(models::decoder()
-                      ("type", "transformer"))
-           .construct();
+    return models::encoder_decoder()(options)
+        .push_back(models::encoder()("type", "transformer"))
+        .push_back(models::decoder()("type", "transformer"))
+        .construct();
   }
 
   if(type == "transformer_gru") {
-    return models::encoder_decoder()
-           (options)
-           .push_back(models::encoder()
-                      ("type", "transformer_gru"))
-           .push_back(models::decoder()
-                      ("type", "transformer_gru"))
-           .construct();
+    return models::encoder_decoder()(options)
+        .push_back(models::encoder()("type", "transformer_gru"))
+        .push_back(models::decoder()("type", "transformer_gru"))
+        .construct();
   }
 
   if(type == "transformer_s2s") {
-    return models::encoder_decoder()
-           (options)
-           .push_back(models::encoder()
-                      ("type", "transformer"))
-           .push_back(models::decoder()
-                      ("type", "s2s"))
-           .construct();
+    return models::encoder_decoder()(options)
+        .push_back(models::encoder()("type", "transformer"))
+        .push_back(models::decoder()("type", "s2s"))
+        .construct();
   }
 
   if(type == "lm") {
-    return models::encoder_decoder()
-           (options)
-           ("type", "s2s")
-           .push_back(models::decoder()
-                      ("index", options->has("index") ?
-                                options->get<size_t>("index") : 0))
-           .construct();
+    auto idx = options->has("index") ? options->get<size_t>("index") : 0;
+    return models::encoder_decoder()(options)("type", "s2s")
+        .push_back(models::decoder()("index", idx))
+        .construct();
   }
 
   if(type == "multi-s2s") {
     size_t numEncoders = 2;
-    auto ms2sFactory = models::encoder_decoder()
-                       (options)
-                       ("type", "s2s");
+    auto ms2sFactory = models::encoder_decoder()(options)("type", "s2s");
 
-    for(size_t i = 0; i < numEncoders; ++i)
-      ms2sFactory.push_back(models::encoder()
-                            ("prefix", "encoder" + std::to_string(i + 1))
-                            ("index", i));
+    for(size_t i = 0; i < numEncoders; ++i) {
+      auto prefix = "encoder" + std::to_string(i + 1);
+      ms2sFactory.push_back(models::encoder()("prefix", prefix)("index", i));
+    }
 
-    ms2sFactory.push_back(models::decoder()
-                          ("index", numEncoders));
+    ms2sFactory.push_back(models::decoder()("index", numEncoders));
 
     return ms2sFactory.construct();
   }
@@ -204,5 +179,4 @@ Ptr<ModelBase> from_config(Ptr<Config> config) {
 }
 
 }
-
 }
