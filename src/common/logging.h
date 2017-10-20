f017e94d894d8bcc1ba0477205cb0fe58814d9eb
Roman Grundkiewicz <romang@amu.edu.pl>
2017-10-20 22:36:31 +0200

    Add new LOG macros

--- src/common/logging.h
+++ src/common/logging.h
@@ -4,6 +4,10 @@
 
 #define LOG(logger) checkedLog(#logger)
 
+#define LOG2(level, ...) checkedLog2("info", #level, __VA_ARGS__)
+
+#define LOG2_VALID(level, ...) checkedLog2("valid", #level, __VA_ARGS__)
+
 typedef std::shared_ptr<spdlog::logger> Logger;
 Logger stderrLogger(const std::string&,
                     const std::string&,
@@ -14,6 +18,30 @@ namespace marian {
 class Config;
 }
 
-// template <class... Args>
+// TODO: remove
 Logger checkedLog(std::string logger);
+
+template <class... Args>
+void checkedLog2(std::string logger, std::string level, Args... args) {
+  Logger log = spdlog::get(logger);
+  if(!log)
+    return;
+
+  if(level == "trace")
+    log->trace(args...);
+  else if(level == "debug")
+    log->debug(args...);
+  else if(level == "info")
+    log->info(args...);
+  else if(level == "warn")
+    log->warn(args...);
+  else if(level == "error")
+    log->error(args...);
+  else if(level == "critical")
+    log->critical(args...);
+  else {
+    log->warn("Unknown log level '{}' for logger '{}'", level, logger);
+  }
+}
+
 void createLoggers(const marian::Config* options = nullptr);
