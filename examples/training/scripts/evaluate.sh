59e0f2c14db40ca16685d2c25fb199d5a54c9d0a
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-11-10 09:14:23 +0000

    Suppress stderr in validation scripts

--- examples/training/scripts/evaluate.sh
+++ examples/training/scripts/evaluate.sh
@@ -2,7 +2,7 @@
 
 cat $1 \
     | sed 's/\@\@ //g' \
-    | moses-scripts/scripts/recaser/detruecase.perl \
-    | moses-scripts/scripts/tokenizer/detokenizer.perl -l ro \
+    | moses-scripts/scripts/recaser/detruecase.perl 2> /dev/null \
+    | moses-scripts/scripts/tokenizer/detokenizer.perl -l ro 2>/dev/null \
     | moses-scripts/scripts/generic/multi-bleu-detok.perl data/newsdev2016.en \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
--- examples/transformer/scripts/evaluate.sh
+++ examples/transformer/scripts/evaluate.sh
@@ -2,7 +2,7 @@
 
 cat $1 \
     | sed 's/\@\@ //g' \
-    | moses-scripts/scripts/recaser/detruecase.perl \
-    | moses-scripts/scripts/tokenizer/detokenizer.perl -l en \
+    | moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
+    | moses-scripts/scripts/tokenizer/detokenizer.perl -l de 2>/dev/null \
     | moses-scripts/scripts/generic/multi-bleu-detok.perl data/valid.de \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
