6b79df21d2d3bcf42aac97c0138cf57ac8f18d9b
Hieu Hoang <hieuhoang@gmail.com>
2017-11-22 19:26:41 +0000

    delete unused functions

--- src/amun/gpu/mblas/array.h
+++ src/amun/gpu/mblas/array.h
@@ -55,34 +55,21 @@ public:
 	cudaFree(m_arr);
   }
 
-  __host__ __device__
   size_t size() const
   { return m_size; }
 
-  __host__
   T *data()
   { return m_arr; }
 
-  __host__
   const T *data() const
   { return m_arr; }
 
-  __device__
-  const T& operator[](size_t ind) const
-  { return m_arr[ind]; }
-
-  __device__
-  T& operator[](size_t ind)
-  { return m_arr[ind]; }
-
-  __host__
   void resize(size_t newSize)
   {
     reserve(newSize);
     m_size = newSize;
   }
 
-  __host__
   void reserve(size_t newSize)
   {
     if (newSize > m_maxSize) {
@@ -96,7 +83,6 @@ public:
     }
   }
 
-  __device__
   void clear()
   {
     m_size = 0;
