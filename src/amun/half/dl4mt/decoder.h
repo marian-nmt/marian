85d111e308492d026c4e1128419e5bf0b80148f8
Hieu Hoang <hieuhoang@gmail.com>
2017-12-04 17:27:56 +0000

    c++14 again

--- src/amun/half/dl4mt/decoder.h
+++ src/amun/half/dl4mt/decoder.h
@@ -203,11 +203,12 @@ class Decoder {
 
           Broadcast(Tanh(_1 + _2), Temp1_, SCU_, Temp2_, dBatchMapping_, maxLength);
 
+          /*
           std::cerr << "Temp1_=" << Temp1_.Debug(0) << std::endl;
           std::cerr << "SCU_=" << SCU_.Debug(0) << std::endl;
           std::cerr << "Temp2_=" << Temp2_.Debug(0) << std::endl;
           std::cerr << "dBatchMapping_=" << dBatchMapping_.Debug(0) << std::endl;
-
+          */
           //std::cerr << "w_.V_=" << w_.V_->Debug(0) << std::endl;
           //std::cerr << "3Temp1_=" << Temp1_.Debug(0) << std::endl;
 
