61fe0b14cc5ba359f9bfedfb86775d4a8bc93f3a
Hieu Hoang <hieuhoang@gmail.com>
2017-12-04 14:32:04 +0000

    use indices

--- src/amun/gpu/mblas/matrix_functions.h
+++ src/amun/gpu/mblas/matrix_functions.h
@@ -151,16 +151,16 @@ __global__ void gBroadcast(Functor functor,
                            const MatrixWrapper<float> in2Wrap,
                            const VectorWrapper<uint> batchMappingWrap)
 {
-  size_t srcSize = outWrap.dim(0);
-  size_t inRows = in2Wrap.dim(0);
-  size_t cols  = in1Wrap.dim(1);
+  //size_t srcSize = outWrap.dim(0);
+  //size_t inRows = in2Wrap.dim(0);
+  //size_t cols  = in1Wrap.dim(1);
 
   int id = threadIdx.x + blockIdx.x * blockDim.x;
   if (id < outWrap.size()) {
-    /*
-    size_t indices[SHAPE_SIZE];
+    uint indices[SHAPE_SIZE];
     outWrap.id2Indices(id, indices);
 
+    /*
     int row = id / cols; // len * batch for in1
     int srcId = row % srcSize;  // source pos for in1
 
@@ -171,18 +171,25 @@ __global__ void gBroadcast(Functor functor,
                           in2Wrap(batchMappingIdx, indices[1], 0, 0) );
     */
 
-    int row = id / cols;
-    int stateIdx = id % cols;
+    //int row = id / cols;
+    //int stateIdx = id % cols;
 
-    int beamIdx = row / srcSize;
-    int srcId = row % srcSize;
+    //int beamIdx = row / srcSize;
+    //int srcId = row % srcSize;
 
-    int batchIdx = batchMappingWrap[beamIdx];
+    uint batchIdx = batchMappingWrap[ indices[2] ];
 
-    outWrap[id] = functor(in1Wrap[(batchIdx * srcSize + srcId) * cols + stateIdx],
-                          in2Wrap[beamIdx * cols + stateIdx]);
-    //outWrap(srcId, stateIdx, beamIdx, 0) = functor(in1Wrap(srcId, stateIdx, 0, batchIdx),
-    //                                              in2Wrap(beamIdx, stateIdx, 0, 0));
+    //assert(srcId == indices[0]);
+    //assert(stateIdx == indices[1]);
+    //assert(beamIdx == indices[2]);
+    //assert(0 == indices[3]);
+
+    //outWrap[id] = functor(in1Wrap[(batchIdx * srcSize + srcId) * cols + stateIdx],
+    //                      in2Wrap[beamIdx * cols + stateIdx]);
+    outWrap[id] = functor(in1Wrap(indices[0], indices[1], 0, batchIdx),
+                          in2Wrap(indices[2], indices[1], 0, 0));
+    //outWrap(indices[0], indices[1], indices[2], 0) = functor(in1Wrap(indices[0], indices[1], 0, batchIdx),
+    //                                              in2Wrap(indices[2], indices[1], 0, 0));
   }
 }
 
