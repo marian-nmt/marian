d51e21044201be6b34dc9b2ef463a829acf224aa
Hieu Hoang <hieuhoang@gmail.com>
2017-12-15 22:47:01 +0000

    delete Debug() for beam

--- src/amun/common/beam.h
+++ src/amun/common/beam.h
@@ -11,8 +11,5 @@ namespace amunmt {
 typedef std::vector<HypothesisPtr> Beam;
 typedef std::vector<Beam> Beams;
 
-std::string Debug(const Beam &vec, size_t verbosity = 1);
-std::string Debug(const Beams &vec, size_t verbosity = 1);
-
 }
 
