a6c827cb02e5bf0fc4dd5596b1c8bd4ef0e314e4
Hieu Hoang <hieuhoang@gmail.com>
2018-01-03 18:11:33 +0000

    use Shape's strides

--- src/amun/gpu/mblas/matrix_wrapper.h
+++ src/amun/gpu/mblas/matrix_wrapper.h
@@ -19,7 +19,6 @@ public:
     dim_[1] = 0;
     dim_[2] = 0;
     dim_[3] = 0;
-    updateStrides();
 
     data_ = nullptr;
     dataConst_ = nullptr;
@@ -33,13 +32,6 @@ public:
     dim_[2] = matrix.dim(2);
     dim_[3] = matrix.dim(3);
 
-    if (colMajor) {
-      updateStrides();
-    }
-    else {
-      updateStridesRowMajor();
-    }
-
     data_ = nullptr;
     dataConst_ = matrix.data();
   }
@@ -52,13 +44,6 @@ public:
     dim_[2] = matrix.dim(2);
     dim_[3] = matrix.dim(3);
 
-    if (colMajor) {
-      updateStrides();
-    }
-    else {
-      updateStridesRowMajor();
-    }
-
     data_ = matrix.data();
     dataConst_ = data_;
   }
@@ -70,7 +55,6 @@ public:
     dim_[1] = b;
     dim_[2] = c;
     dim_[3] = d;
-    updateStrides();
   }
 
   __device__
@@ -81,7 +65,6 @@ public:
     dim_[1] = b;
     dim_[2] = c;
     dim_[3] = d;
-    updateStrides();
 
     data_ = ptr;
     dataConst_ = ptr;
@@ -90,32 +73,6 @@ public:
   __device__ __host__
   const Shape &GetShape() const
   { return shape_; }
-
-  __device__ __host__
-  unsigned stride(unsigned i) const
-  {
-    return stride_[i];
-  }
-
-  __device__ __host__
-  void updateStrides()
-  {
-    stride_[0] = dim_[1];
-    stride_[1] = 1;
-    stride_[2] = dim_[0] * dim_[1];
-    stride_[3] = dim_[0] * dim_[1] * dim_[2];
-
-  }
-
-  __device__ __host__
-  void updateStridesRowMajor()
-  {
-    stride_[0] = 1;
-    stride_[1] = dim_[0];
-    stride_[2] = dim_[0] * dim_[1];
-    stride_[3] = dim_[0] * dim_[1] * dim_[2];
-
-  }
 
   __device__
   T* data()
@@ -222,7 +179,6 @@ public:
 
 protected:
   unsigned dim_[SHAPE_SIZE];
-  unsigned stride_[SHAPE_SIZE];
   Shape shape_;
 
   T *data_;
