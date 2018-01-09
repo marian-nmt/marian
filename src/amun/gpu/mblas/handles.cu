712eb1b7205e44739a2e9c52ea57459697b975cb
Hieu Hoang <hieuhoang@gmail.com>
2018-01-09 22:32:02 +0000

    enable tensor core

--- src/amun/gpu/mblas/handles.cu
+++ src/amun/gpu/mblas/handles.cu
@@ -25,14 +25,22 @@ CublasHandler::CublasHandler()
   cublasStatus_t stat;
   stat = cublasCreate(&handle_);
   if (stat != CUBLAS_STATUS_SUCCESS) {
-  printf ("cublasCreate initialization failed\n");
-  abort();
+    printf ("cublasCreate initialization failed\n");
+    abort();
   }
+
+#if CUDA_VERSION >= 9000
+  stat = cublasSetMathMode(handle_, CUBLAS_TENSOR_OP_MATH);
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    printf ("cublasSetMathMode failed\n");
+    abort();
+  }
+#endif
 
   stat = cublasSetStream(handle_, CudaStreamHandler::GetStream());
   if (stat != CUBLAS_STATUS_SUCCESS) {
-  printf ("cublasSetStream initialization failed\n");
-  abort();
+    printf ("cublasSetStream initialization failed\n");
+    abort();
   }
 }
 
