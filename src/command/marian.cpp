0a1cdbc9d502a1d8d6722db73e72fe1c55c16205
r0452425 <tomneckermann@gmail.com>
2018-01-21 07:06:51 +0000

    Add logic and compile config for multi-node

--- src/command/marian.cpp
+++ src/command/marian.cpp
@@ -4,7 +4,10 @@
 #include "training/graph_group_async_drop.h"
 #include "training/graph_group_singleton.h"
 #include "training/graph_group_sync.h"
+#include "training/graph_group_multinode.h"
 #include "training/training.h"
+
+bool configureMPI(int, char**);
 
 int main(int argc, char** argv) {
   using namespace marian;
@@ -12,16 +15,33 @@ int main(int argc, char** argv) {
   auto options = New<Config>(argc, argv);
   auto devices = options->get<std::vector<size_t>>("devices");
 
-  if(devices.size() > 1) {
+  bool useMultiNode = options->get<bool>("multi-node") && configureMPI(argc, argv);
+
+  if(devices.size() > 1 && !useMultiNode) {
     if(options->get<bool>("sync-sgd"))
       New<Train<SyncGraphGroup>>(options)->run();
     else if(options->get<float>("grad-dropping-rate") > 0.0)
       New<Train<AsyncGraphGroupDrop>>(options)->run();
     else
       New<Train<AsyncGraphGroup>>(options)->run();
-  } else {
+  } else if(!useMultiNode) {
     New<Train<SingletonGraph>>(options)->run();
+  } else {
+    LOG(info, "[BETA] Running multi-node training.");
+    New<Train<MultiNodeGraphGroup>>(options)->run();
   }
 
   return 0;
+}
+
+bool configureMPI(int argc, char** argv) {
+  bool enable = false;
+  #if MPI_FOUND
+  int provided_thread_mode = 0;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided_thread_mode);
+  MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN); // Enable if occasional truncation errors
+  ABORT_IF(provided_thread_mode < MPI_THREAD_MULTIPLE, "Your version of MPI does not support multi-threaded communication.");
+  enable = true;
+  #endif
+  return enable;
 }
