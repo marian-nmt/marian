7d239425d030318281d32d152b2d0d20848a7602
Hieu Hoang <hieuhoang@gmail.com>
2018-01-25 15:16:18 +0000

    call opencl kernel

--- contrib/fpga/main.cpp
+++ contrib/fpga/main.cpp
@@ -9,6 +9,66 @@
 
 using namespace std;
 
+/////////////////////////////////////////////////////////////////////////////////////
+template <typename T>
+void SetKernelArg(cl_kernel kernel, cl_uint argNum, const T &t)
+{
+  //std::cerr << "arg" << argNum << "=" << t << std::endl ;
+  CheckError( clSetKernelArg(kernel, argNum, sizeof(T), &t) );
+}
+
+template<typename T, typename... Args>
+void SetKernelArg(cl_kernel kernel, cl_uint argNum, const T &t, Args... args) // recursive variadic function
+{
+  //std::cerr << "arg" << argNum << "=" << t << std::endl ;
+  CheckError( clSetKernelArg(kernel, argNum, sizeof(T), &t) );
+
+  SetKernelArg(kernel, argNum + 1, args...) ;
+}
+
+template<typename... Args>
+void CallOpenCL(
+    const std::string &filePath,
+    const std::string &kernelName,
+    const OpenCLInfo &openCLInfo,
+    Args... args
+    )
+{
+  cl_int err;
+  size_t global;                      // global domain size for our calculation
+  size_t local;                       // local domain size for our calculation
+
+  cl_mem output = clCreateBuffer(openCLInfo.context, CL_MEM_WRITE_ONLY, sizeof(size_t), NULL, &err);
+  CheckError(err);
+  assert(output);
+
+  // create kernel
+  cl_kernel kernel = CreateKernel(filePath, kernelName, openCLInfo);
+
+  // Set the arguments to our compute kernel
+  SetKernelArg(kernel, 0, args...);
+
+  // Get the maximum work group size for executing the kernel on the device
+  //
+  CheckError( clGetKernelWorkGroupInfo(kernel, openCLInfo.device, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL) );
+
+  //global = 1024;
+  local = 1;
+  global = 1;
+
+  //cerr << "local=" << local << endl;
+  //cerr << "global=" << global << endl;
+
+  CheckError( clEnqueueNDRangeKernel(openCLInfo.commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL) );
+
+  // Wait for the command commands to get serviced before reading back results
+  //
+  CheckError( clFinish(openCLInfo.commands) );
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
   cerr << "Starting..." << endl;
@@ -40,6 +100,9 @@ int main()
 
   vec.resize(B.size(), 9.3443);
   B.Set(vec.data(), vec.size());
+
+  CallOpenCL("kernels/matrix_functions.cl", "sum_float", openCLInfo,
+      X.data(), Y.data(), X.size());
 
   cerr << "Finished" << endl;
 }
