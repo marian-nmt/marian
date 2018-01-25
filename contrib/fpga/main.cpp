c6a177614c554d4bc0559ac3cb366f93c452bc26
Hieu Hoang <hieuhoang@gmail.com>
2018-01-25 14:59:10 +0000

    fpga fusion test

---
+++ contrib/fpga/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include "types-fpga.h"
+#include "kernel.h"
+#include "matrix.h"
+
+using namespace std;
+
+int main()
+{
+  cerr << "Starting..." << endl;
+
+  OpenCLInfo openCLInfo;
+
+  openCLInfo.context = CreateContext(100, openCLInfo.devices, openCLInfo.numDevices);
+  cerr << "CreateContext done" << endl;
+
+  openCLInfo.device = openCLInfo.devices[0];
+  openCLInfo.commands = CreateCommandQueue(openCLInfo);
+  cerr << "CreateCommandQueue done" << endl;
+
+  cl_kernel kernel = CreateKernel("kernels/OutputLayer.cl", "square", openCLInfo);
+  cerr << "CreateKernel done" << endl;
+
+  Matrix W(openCLInfo, true, 85000, 512);
+  Matrix X(openCLInfo, true, 512, 640);
+  Matrix B(openCLInfo, true, 1, 85000);
+  Matrix Y(openCLInfo, true, 85000, 640);
+
+  vector<float> vec(85000*512, 3.3);
+  W.Set(vec.data(), vec.size());
+
+  cerr << "Finished" << endl;
+}
+
