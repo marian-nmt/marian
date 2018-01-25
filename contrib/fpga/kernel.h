e0836551330f5ada4ffc2a4195acd940ebf99762
Hieu Hoang <hieuhoang@gmail.com>
2018-01-25 11:49:53 +0000

    opencl test

---
+++ contrib/fpga/kernel.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include "types-fpga.h"
+
+cl_context CreateContext(
+    size_t maxDevices,
+    cl_device_id *devices,
+    cl_uint &numDevices);
+
+cl_kernel CreateKernel(const std::string &filePath, const std::string &kernelName, const OpenCLInfo &openCLInfo);
+cl_command_queue CreateCommandQueue(const OpenCLInfo &openCLInfo);
+
+
+
