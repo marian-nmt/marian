4984e7fcacf04706cd0d2dd5e6a6f3521ded8297
Hieu Hoang <hieuhoang@gmail.com>
2018-01-25 14:31:59 +0000

    delete strides. Not needed

--- contrib/fpga/matrix.h
+++ contrib/fpga/matrix.h
@@ -11,13 +11,6 @@ public:
     dim_[1] = b;
     size_ = a * b;
 
-    if (rowMajor) {
-      updateStridesRowMajor();
-    }
-    else {
-      updateStridesColMajor();
-    }
-
     cl_int err;
     mem_ = clCreateBuffer(openCLInfo.context,  CL_MEM_READ_WRITE,  sizeof(float) * size(), NULL, &err);
     CheckError(err);
@@ -35,43 +28,14 @@ public:
   unsigned dim(unsigned i) const
   { return dim_[i]; }
 
-  unsigned stride(unsigned i) const
-  {  return stride_[i]; }
-
   unsigned size() const
   { return size_; }
 
 
-  unsigned indices2Id(unsigned a, unsigned b) const
-  {
-    assert(a < dim(0));
-    assert(b < dim(1));
-  
-    unsigned ind =
-            a * stride(0)
-          + b * stride(1);
-    assert(ind < size());
-    return ind;
-  }
-
 protected:
   bool rowMajor_;
   unsigned dim_[2];
-  unsigned stride_[2];
   unsigned size_;
   cl_mem mem_;
-
-  void updateStridesRowMajor()
-  {
-    stride_[0] = 1;
-    stride_[1] = dim_[0];
-  }
-
-  void updateStridesColMajor()
-  {
-    stride_[0] = dim_[1];
-    stride_[1] = 1;
-  }
-
 };
 
