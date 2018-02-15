d565d522129328aa6cae3974d49843ffdcd3d9aa
Frank Seide <fseide@microsoft.com>
2018-02-14 17:11:25 -0800

    removed the last CNTK-specific #ifdef

--- src/models/model_base.h
+++ src/models/model_base.h
@@ -8,7 +8,6 @@ namespace models {
 
 class ModelBase {
 public:
-#ifndef CNTK_BACKEND
   virtual void load(Ptr<ExpressionGraph>,
                     const std::string&,
                     bool markReloaded = true)
@@ -17,18 +16,15 @@ public:
                     const std::string&,
                     bool saveTranslatorConfig = false)
       = 0;
-#endif
 
   virtual Expr build(Ptr<ExpressionGraph> graph,
                      Ptr<data::Batch> batch,
                      bool clearGraph = true)
       = 0;
 
-#ifndef CNTK_BACKEND
   virtual Ptr<data::BatchStats> collectStats(Ptr<ExpressionGraph> graph,
                                              size_t multiplier = 1)
       = 0;
-#endif
 
   virtual void clear(Ptr<ExpressionGraph> graph) = 0;
 };
