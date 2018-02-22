8d3f70599f3ff3944a158948ea9e147744909048
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2018-02-21 22:08:36 -0800

    batched sgemm

--- src/tensors/cpu/prod.cpp
+++ src/tensors/cpu/prod.cpp
@@ -66,7 +66,63 @@ void ProdBatched(marian::Tensor C,
                  bool transB,
                  float beta,
                  float scalar) {
+#if BLAS_FOUND
+  float alpha = scalar;
+  
+  size_t batchA = A->shape().elements() / (A->shape()[-1] * A->shape()[-2]);
+  size_t batchB = B->shape().elements() / (B->shape()[-1] * B->shape()[-2]);
+
+  size_t m = A->shape()[-2];
+  size_t k = A->shape()[-1];
+  if(transA)
+    std::swap(m, k);
+
+  size_t l = B->shape()[-2];
+  size_t n = B->shape()[-1];
+  if(transB)
+    std::swap(l, n);
+
+  size_t lda = A->shape()[-1];
+  size_t ldb = B->shape()[-1];
+  size_t ldc = B->shape()[-1];
+
+  if(transB)
+    ldc = B->shape()[-2];
+
+  auto opA = transA ? CblasTrans : CblasNoTrans;
+  auto opB = transB ? CblasTrans : CblasNoTrans;  
+    
+  auto strideB = batchB == 1 ? 0 : n * k;
+  auto strideA = batchA == 1 ? 0 : m * k;
+  auto strideC = n * m;
+  
+  int steps = std::max(batchA, batchB);
+  
+  int offsetA = 0;
+  int offsetB = 0;
+  int offsetC = 0;
+  for(int i = 0; i < steps; ++i) {
+    cblas_sgemm(
+          CblasColMajor,
+          opB,
+          opA,
+          n, m, k,
+          alpha,
+          B->data() + offsetB,
+          ldb,
+          A->data() + offsetA,
+          lda,
+          beta,
+          C->data() + offsetC,
+          ldc);
+    
+    offsetA += strideA;
+    offsetB += strideB;
+    offsetC += strideC;
+  }
+#else
   ABORT("Not implemented!");
+#endif
 }
 
 }
