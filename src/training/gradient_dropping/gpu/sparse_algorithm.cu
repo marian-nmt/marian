76a7f2abdbcf3844f422d8491154c40e2cd3df40
Alham Fikri Aji <afaji321@gmail.com>
2018-03-10 10:18:03 +0000

    fix spacing

--- src/training/gradient_dropping/gpu/sparse_algorithm.cu
+++ src/training/gradient_dropping/gpu/sparse_algorithm.cu
@@ -12,38 +12,38 @@
 
 namespace marian {
   namespace gpu {
-  	struct non_zero
-	{
-	    __host__ __device__
-	    bool operator()(const float x)
-	    {
-	        return x != 0;
-	    }
-	};
+    struct non_zero
+    {
+        __host__ __device__
+        bool operator()(const float x)
+        {
+            return x != 0;
+        }
+    };
 
-	__global__ void copy_id(float* data,
-	                        int* indices,
-	                        float* out,
-	                        int size) {
-	  int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	  if(idx >= size)
-	    return;
-	  out[idx] = data[indices[idx]];
-	}
+    __global__ void copy_id(float* data,
+                            int* indices,
+                            float* out,
+                            int size) {
+      int idx = blockDim.x * blockIdx.x + threadIdx.x;
+      if(idx >= size)
+        return;
+      out[idx] = data[indices[idx]];
+    }
 
-	__global__ void gScatterAdd(float* denseData,
+    __global__ void gScatterAdd(float* denseData,
                             float* sparseData,
                             int* sparseIndices,
                             int denseSize,
                             int sparseSize,
                             int offset) {
-	  int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	  if(idx >= sparseSize)
-	    return;
-	  if(sparseIndices[idx] >= -offset
-	     && sparseIndices[idx] + offset < denseSize)
-	    denseData[sparseIndices[idx] + offset] += sparseData[idx];
-	}
+      int idx = blockDim.x * blockIdx.x + threadIdx.x;
+      if(idx >= sparseSize)
+        return;
+      if(sparseIndices[idx] >= -offset
+         && sparseIndices[idx] + offset < denseSize)
+        denseData[sparseIndices[idx] + offset] += sparseData[idx];
+    }
 
 
     std::vector<int> lower_bounds(int* data, std::vector<int> values, int size, DeviceId device) {  
@@ -57,43 +57,43 @@ namespace marian {
                           d_values.begin(), d_values.end(),
                           d_output.begin());
 
-   	  std::vector<int> output(values.size());
-   	  thrust::copy(d_output.begin(), d_output.end(), output.begin());
+         std::vector<int> output(values.size());
+         thrust::copy(d_output.begin(), d_output.end(), output.begin());
 
-   	  return output;
+         return output;
     }
 
 
     int buildSparse(Tensor t, float* data, int* indices) {
-	  cudaSetDevice(t->getDevice().no);
-	  using namespace thrust;
+      cudaSetDevice(t->getDevice().no);
+      using namespace thrust;
 
-	  device_ptr<float> grad_ptr(t->data());
-	  device_ptr<float> sparse_grad_ptr(data);
-	  device_ptr<int> indices_ptr(indices);
+      device_ptr<float> grad_ptr(t->data());
+      device_ptr<float> sparse_grad_ptr(data);
+      device_ptr<int> indices_ptr(indices);
 
-	  int sparse_size = copy_if(make_counting_iterator<int>(0), 
-	    make_counting_iterator<int>(t->size()), 
-	    grad_ptr, 
-	    indices_ptr, 
-	    non_zero()) - indices_ptr;
+      int sparse_size = copy_if(make_counting_iterator<int>(0), 
+        make_counting_iterator<int>(t->size()), 
+        grad_ptr, 
+        indices_ptr, 
+        non_zero()) - indices_ptr;
 
-	  int threads = 512;
-	  int blocks = 1 + t->size() / threads;
-	  copy_id<<<blocks, threads>>>(t->data(), indices, data, sparse_size);
+      int threads = 512;
+      int blocks = 1 + t->size() / threads;
+      copy_id<<<blocks, threads>>>(t->data(), indices, data, sparse_size);
 
-	  return sparse_size;
+      return sparse_size;
     }
 
 
     void scatterAdd(Tensor t, float* data, int *indices, int size, int offset) {
       cudaSetDevice(t->getDevice().no);
 
-	  int threads = 512;
-	  int blocks = 1 + size / threads;
-	  gScatterAdd<<<blocks, threads>>>(
-	      t->data(), data, indices, t->size(), size, offset);
-	  cudaStreamSynchronize(0);
+      int threads = 512;
+      int blocks = 1 + size / threads;
+      gScatterAdd<<<blocks, threads>>>(
+          t->data(), data, indices, t->size(), size, offset);
+      cudaStreamSynchronize(0);
     }
   }
 }
