548d71497c7df7867a7fb09018b6132aebb35cb5
Alham Fikri Aji <afaji321@gmail.com>
2018-03-10 10:25:06 +0000

    fix spacing

--- src/training/gradient_dropping/gpu/sparse_algorithm.h
+++ src/training/gradient_dropping/gpu/sparse_algorithm.h
@@ -6,15 +6,15 @@
 
 
 namespace marian {
-	namespace gpu {
-		// output is a vector of size values.size. Output[i] is lower_bound of values[i] in data
-		std::vector<int> lower_bounds(int* data,
-									  std::vector<int> values,
-									  int size,
-									  DeviceId device);
+  namespace gpu {
+    // output is a vector of size values.size. Output[i] is lower_bound of values[i] in data
+    std::vector<int> lower_bounds(int* data,
+                                  std::vector<int> values,
+                                  int size,
+                                  DeviceId device);
 
-		int buildSparse(Tensor t, float* data, int* indices);
+    int buildSparse(Tensor t, float* data, int* indices);
 
-		void scatterAdd(Tensor t, float* data, int *indices, int size, int offset);
-	}
+    void scatterAdd(Tensor t, float* data, int *indices, int size, int offset);
+  }
 }
