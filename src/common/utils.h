7447dcba4fe3a0de4920d740658df18b3cf77b92
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2018-06-07 15:14:54 -0700

    small fixes to make things compile and pass regression tests

--- src/common/utils.h
+++ src/common/utils.h
@@ -29,3 +29,14 @@ std::basic_istream<CharT,Traits>& GetLine(std::basic_istream<CharT,Traits>& in,
     line.pop_back();
   return in;
 }
+
+// wrapper around std::getline() that handles Windows input files with extra CR chars at the line end
+template< class CharT, class Traits, class Allocator >
+std::basic_istream<CharT,Traits>& GetLine(std::basic_istream<CharT,Traits>& in,
+                                          std::basic_string<CharT,Traits,Allocator>& line,
+                                          CharT delim) {
+  std::getline(in, line, delim);
+  if (in && !line.empty() && line.back() == in.widen('\r')) // strip terminal CR if present
+    line.pop_back();
+  return in;
+}
