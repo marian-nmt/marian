9beab3e6bb28a5c8ef90b118731273e52a177640
Frank Seide <fseide@microsoft.com>
2018-06-06 17:21:37 -0700

    temporarily disabled an error dialog box caused by an unaligned free()

--- src/command/marian_decoder.cpp
+++ src/command/marian_decoder.cpp
@@ -1,6 +1,9 @@
 #include "marian.h"
 #include "translator/beam_search.h"
 #include "translator/translator.h"
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 int main(int argc, char** argv) {
   using namespace marian;
@@ -11,6 +14,10 @@ int main(int argc, char** argv) {
   boost::timer::cpu_timer timer;
   task->run();
   LOG(info, "Total time: {}", timer.format());
+
+#ifdef _WIN32 // debug CRT throws an error that free() is used for an aligned allocation; disable this error for now
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif
 
   return 0;
 }
