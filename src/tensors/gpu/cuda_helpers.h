112118fce3c8c54049913126c2685e8e7463713c
Cédric Rousseau <cedrou@gmail.com>
2018-09-04 17:49:30 +0200

    Fix error : identifier "CUDA_FLT_MAX" is undefined in device code

--- src/tensors/gpu/cuda_helpers.h
+++ src/tensors/gpu/cuda_helpers.h
@@ -4,7 +4,7 @@
 #include "3rd_party/exception.h"
 #include "common/logging.h"
 
-const float CUDA_FLT_MAX = 1.70141e+38;
+__constant__ float CUDA_FLT_MAX = 1.70141e+38;
 const int MAX_THREADS = 512;
 const int MAX_BLOCKS = 65535;
 
