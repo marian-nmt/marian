6835401d6b06adc19cbda3fb046869efe23ff234
Roman Grundkiewicz <rgrundki@ex.ed.ac.uk>
2018-10-09 12:27:32 +0000

    Fix async training with exponential smoothing

--- src/training/graph_group_async.cpp
+++ src/training/graph_group_async.cpp
@@ -152,6 +152,7 @@ void AsyncGraphGroup::init(Ptr<data::Batch> batch) {
     int totalSize = (int)graphs_[0]->params()->vals()->size();
 
     int i = 0;
+    int pos = 0;
     for(auto graph : graphs_) {
       int __size__ = std::min(shardSize_, totalSize);
       totalSize -= __size__;
@@ -163,12 +164,14 @@ void AsyncGraphGroup::init(Ptr<data::Batch> batch) {
       allocator->allocate(paramAvg, {1, __size__});
 
       if(graphAvg)
-        paramAvg->copyFrom(graphAvg->params()->vals());
+        paramAvg->copyFrom(graphAvg->params()->vals()->subtensor(pos, __size__));
       else
         paramAvg->copyFrom(params_[i++]);
 
       paramsAllocAvg_.push_back(allocator);
       paramsAvg_.push_back(paramAvg);
+
+      pos += __size__;
     }
 
     if(graphAvg)
