7ffd8d301b80a57e77f88b97f1efe86d987d7545
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-11-07 09:29:46 +0000

    Abort if an unknown option comes from a config file

--- src/common/cli_wrapper.cpp
+++ src/common/cli_wrapper.cpp
@@ -1,4 +1,5 @@
 #include "common/cli_wrapper.h"
+#include "common/logging.h"
 #include "common/options.h"
 #include "common/version.h"
 
@@ -141,7 +142,7 @@ void CLIWrapper::overwriteDefault(const YAML::Node &node) {
     // warn if the option for which the default value we are setting for has
     // been not defined
     if(allVars_.count(key) == 0)
-      LOG(warn, "Default value for an undefined option with key '{}'", key);
+      ABORT_IF(!allVars_.count(key), "The following option was not expected: '{}'", key);
     // if we have an option but it was not specified on command-line
     if(allVars_.count(key) > 0 && opts_.at(key)->empty()) {
       config_[key] = YAML::Clone(it.second);
