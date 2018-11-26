9cce5d255cfaf95fcbd728f6b5b63df435785f7d
Marcin Junczys-Dowmunt <marcinjd@microsoft.com>
2018-11-25 20:42:56 -0800

    add random generator to shuffle

--- src/data/sentencepiece_vocab.cpp
+++ src/data/sentencepiece_vocab.cpp
@@ -105,7 +105,7 @@ public:
     size_t seenLines = 0;
     for(const auto& trainPath : trainPaths)
       reservoirSampling(sample, seenLines, trainPath, maxLines, maxBytes);
-    std::random_shuffle(sample.begin(), sample.end());
+    std::shuffle(sample.begin(), sample.end(), generator_);
 
     io::OutputFileStream out(temp);
     for(const auto& line : sample)
