7b649c00e0d08083818235533b770cc46c66716e
Frank Seide <fseide@microsoft.com>
2019-01-31 14:45:51 -0800

    In VS Project, changed transformer.h from CPP file to a true header, and added the stub, to fix a compilation warning

--- src/models/transformer_factory.h
+++ src/models/transformer_factory.h
@@ -4,12 +4,8 @@
 
 #include "models/decoder.h"
 #include "models/encoder.h"
-//#include "models/states.h"
-//#include "layers/constructors.h"
-//#include "layers/factory.h"
 
 namespace marian {
-// @TODO: find out why static is required here to get to compile
-static Ptr<EncoderBase> NewEncoderTransformer(Ptr<Options> options);
-static Ptr<DecoderBase> NewDecoderTransformer(Ptr<Options> options);
+Ptr<EncoderBase> NewEncoderTransformer(Ptr<Options> options);
+Ptr<DecoderBase> NewDecoderTransformer(Ptr<Options> options);
 }  // namespace marian
