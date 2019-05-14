4c6a809a2e4f6efec72197e40f921ce9b4cac7cd
Frank Seide <fseide@microsoft.com>
2019-05-14 14:52:50 -0700

    reformatted

--- src/layers/generic.cpp
+++ src/layers/generic.cpp
@@ -461,27 +461,18 @@ namespace marian {
     // create source embeddings
     int dimVoc = opt<std::vector<int>>("dim-vocabs")[batchIndex_];
     int dimEmb = opt<int>("dim-emb");
-
-    // @TODO: code dup with Decoder and EncoderTransformer; actually diverged by now. Unify this.
-    auto embFactory = embedding()  //
-        ("dimVocab", dimVoc)       //
-        ("dimEmb", dimEmb);
-
+    auto embFactory = embedding()("dimVocab", dimVoc)("dimEmb", dimEmb);
     if(opt<bool>("tied-embeddings-src") || opt<bool>("tied-embeddings-all"))
       embFactory("prefix", "Wemb");
     else
       embFactory("prefix", prefix_ + "_Wemb");
-
     if(options_->has("embedding-fix-src"))
       embFactory("fixed", opt<bool>("embedding-fix-src"));
-
     if(options_->hasAndNotEmpty("embedding-vectors")) {
       auto embFiles = opt<std::vector<std::string>>("embedding-vectors");
-      embFactory                              //
-          ("embFile", embFiles[batchIndex_])  //
-          ("normalization", opt<bool>("embedding-normalization"));
+      embFactory("embFile", embFiles[batchIndex_])
+                ("normalization", opt<bool>("embedding-normalization"));
     }
-
     embFactory("vocab", opt<std::vector<std::string>>("vocabs")[batchIndex_]); // for factored embeddings
     return embFactory.construct(graph);
   }
