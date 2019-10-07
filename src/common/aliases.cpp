dfbf4f403a0ab50af028fd3ad7d5fd36c581a2bd
Marcin Junczys-Dowmunt <Marcin.JunczysDowmunt@microsoft.com>
2019-10-07 15:52:24 -0700

    remove wrong abort

--- src/common/aliases.cpp
+++ src/common/aliases.cpp
@@ -32,7 +32,6 @@ void ConfigParser::addAliases(cli::CLIWrapper& cli) {
       // @TODO: review this
       // scaling factor (power of 2), frequency, multiplier at increase, tolerance, range, minium factor
       config["cost-scaling"] = std::vector<std::string>({"7", "2000", "2", "0.05", "10", "1"}); 
-      ABORT("fp16 training currently not yet supported.");
     } else {
       config["precision"] = std::vector<std::string>({"float16"}); // for inference we do not need the other types
     }
