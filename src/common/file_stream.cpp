cf283d03366b5a5b9a5dd8873173b089a40e6ccf
Hieu Hoang <hihoan@microsoft.com>
2019-10-24 13:24:12 -0700

    delete unused includes

--- src/common/file_stream.cpp
+++ src/common/file_stream.cpp
@@ -1,13 +1,1 @@
 #include "common/file_stream.h"
-
-#include <streambuf>
-#include <string>
-#include <vector>
-#ifdef _MSC_VER
-#include <windows.h>
-#include <io.h>
-#else
-#include <sys/types.h>
-#include <unistd.h>
-#endif
-
