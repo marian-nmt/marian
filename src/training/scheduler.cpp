fe72735e3173d8439b8f232c934008fdf77d7f22
Ulrich Germann <ugermann@inf.ed.ac.uk>
2019-10-25 22:25:00 +0100

    Update scheduler.cpp
    As requested by @emjotde

--- src/training/scheduler.cpp
+++ src/training/scheduler.cpp
@@ -5,26 +5,22 @@
 namespace marian {
 bool Scheduler::sigterm_{false};
 
-void
-Scheduler::
-signalHandler_(int sig) {
+void Scheduler::signalHandler(int sig) {
   // Note: sys_siglist[sig] or stdsignal() describe the effect (e.g.,
   // 'Terminated' rather than provide the signal name (which are #define(s)
   // in signal.h), so we have to do custom log messages here.
   switch (sig) {
-  case SIGTERM: // save models and exit
-    LOG(info, "[training] Scheduler received signal SIGTERM");
-    Scheduler::sigterm_ = true;
-    break;
-  default:
-    ABORT("No action defined for signal {}", sig);
+    case SIGTERM: // save models and exit
+      LOG(info, "[training] Scheduler received signal SIGTERM");
+      Scheduler::sigterm_ = true;
+      break;
+    default:
+      ABORT("No action defined for signal {}", sig);
   }
 }
 
 
-void
-Scheduler::
-installSignalHandlers_() {
+void Scheduler::installSignalHandlers() {
   // TODO: use sigaction instead of signal
   signal(SIGTERM, Scheduler::signalHandler_);
 }
