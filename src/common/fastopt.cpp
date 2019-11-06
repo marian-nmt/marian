a8826c5d12bd0eeeb104a374b25e08de9918f79e
Marcin Junczys-Dowmunt <Marcin.JunczysDowmunt@microsoft.com>
2019-11-05 23:53:40 -0800

    make compile with cudnn

--- src/common/fastopt.cpp
+++ src/common/fastopt.cpp
@@ -1,5 +1,7 @@
 #include "common/fastopt.h"
 
+#include <utility>
+
 namespace marian {
 
 // see fastopt.h for comments
@@ -58,6 +60,14 @@ T As<T>::apply(const FastOpt& node) {
   }
 }
 
+// specializations for simple types
+template struct As<bool>;
+template struct As<int>;
+template struct As<unsigned long>;
+template struct As<float>;
+template struct As<double>;
+template struct As<std::string>;
+
 // specialization of above class for std::vector<T>
 template <typename T>
 std::vector<T> As<std::vector<T>>::apply(const FastOpt& node) {
@@ -69,14 +79,6 @@ std::vector<T> As<std::vector<T>>::apply(const FastOpt& node) {
   return seq;
 }
 
-// specializations for simple types
-template struct As<bool>;
-template struct As<int>;
-template struct As<unsigned long>;
-template struct As<float>;
-template struct As<double>;
-template struct As<std::string>;
-
 // specializations for simple vector types
 template struct As<std::vector<bool>>;
 template struct As<std::vector<int>>;
@@ -84,6 +86,16 @@ template struct As<std::vector<unsigned long>>;
 template struct As<std::vector<float>>;
 template struct As<std::vector<double>>;
 template struct As<std::vector<std::string>>;
+
+// specialization of above class for std::pair<T>
+template <typename T1, typename T2>
+std::pair<T1, T2> As<std::pair<T1, T2>>::apply(const FastOpt& node) {
+  ABORT_IF(!node.isSequence(), "Node is not a sequence node");
+  ABORT_IF(node.size() != 2, "Sequence must contain two elements in order to convert to pair");
+  return std::make_pair(node[0].as<T1>(), node[1].as<T2>());
+}
+
+template struct As<std::pair<int, int>>;
 
 }
 }
