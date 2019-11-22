b19820c8ba911ae2e993bf570622af03209d7c15
Frank Seide <fseide@microsoft.com>
2019-11-22 23:18:52 +0000

    Merged PR 10588: bug fix: guided-alignment loss should not normalize by source length
    The guided-alignment loss has two bugs related to two different types of normalization:
    * The returned `RationalLoss` should not have its denominator weighted by `guided-alignment-weight`, except in `sum` mode.
    * The CE loss cannot directly use the alignment labels as ground truth in the CE loss function, since CE requires one-hot labels, while the actual labels those are multi-hot. They must be renormalized.

--- src/layers/guided_alignment.h
+++ src/layers/guided_alignment.h
@@ -1,43 +1,68 @@
 #pragma once
 
 #include "layers/loss.h"
+#include "common/logging.h"
 
 namespace marian {
 
 static inline RationalLoss guidedAlignmentCost(Ptr<ExpressionGraph> graph,
                                                Ptr<data::CorpusBatch> batch,
                                                Ptr<Options> options,
-                                               Expr attention) {
+                                               Expr attention) { // [beam depth=1, max src length, batch size, tgt length]
 
-  // @TODO: there should be positional masking here ... on the other hand, positions that are not 
-  // in a sentence should always agree (both being 0). Lack of masking affects label count only which is 
-  // probably negligible?
-
-  // @TODO: change "cost" to "loss"
-  std::string guidedLossType = options->get<std::string>("guided-alignment-cost");
-  float guidedScalar = options->get<float>("guided-alignment-weight");
+  std::string guidedLossType = options->get<std::string>("guided-alignment-cost");  // @TODO: change "cost" to "loss"
+  float guidedLossWeight = options->get<float>("guided-alignment-weight");
   
+  const auto& shape = attention->shape(); // [beam depth=1, max src length, batch size, tgt length]
   float epsilon = 1e-6f;
-  Expr alignment = constant_like(attention, inits::fromVector(batch->getGuidedAlignment()));
   Expr alignmentLoss; // sum up loss over all attention/alignment positions
-  if(guidedLossType == "mse") {
-    alignmentLoss = sum(flatten(square(attention - alignment))) / 2.f;
-  } else if(guidedLossType == "mult") {
-    alignmentLoss = -log(sum(flatten(attention * alignment)) + epsilon);
-  } else if(guidedLossType == "ce") {
+  size_t numLabels;
+  if(guidedLossType == "ce") {
+    // ce requires normalized probabilities
+    auto dimBatch    = shape[-2];
+    auto dimTrgWords = shape[-1];
+    auto dimSrcWords = shape[-3];
+    ABORT_IF(shape[-4] != 1, "Guided alignments with beam??");
+    auto normalizedAlignment = batch->getGuidedAlignment();
+    auto locate = [=](size_t s, size_t b, size_t t) { return ((s * dimBatch) + b) * dimTrgWords + t; };
+    for (size_t b = 0; b < dimBatch; b++) {
+      for (size_t t = 0; t < dimTrgWords; t++) {
+        float sum = 0;
+        for (size_t s = 0; s < dimSrcWords; s++)
+          sum += normalizedAlignment[locate(s, b, t)];
+        if (sum != 0 && sum != 1)
+          for (size_t s = 0; s < dimSrcWords; s++)
+            normalizedAlignment[locate(s, b, t)] /= sum;
+      }
+    }
+    auto alignment = constant_like(attention, inits::fromVector(normalizedAlignment));
     alignmentLoss = -sum(flatten(alignment * log(attention + epsilon)));
+    numLabels = batch->back()->batchWords();
+    ABORT_IF(numLabels > shape.elements() / shape[-3], "Num labels of guided alignment cost is off??");
   } else {
-    ABORT("Unknown alignment cost type: {}", guidedLossType);
+    auto alignment = constant_like(attention, inits::fromVector(batch->getGuidedAlignment()));
+    if(guidedLossType == "mse")
+      alignmentLoss = sum(flatten(square(attention - alignment))) / 2.f;
+    else if(guidedLossType == "mult") // @TODO: I don't know what this criterion is for. Can we remove it?
+      alignmentLoss = -log(sum(flatten(attention * alignment)) + epsilon);
+    else
+       ABORT("Unknown alignment cost type: {}", guidedLossType);
+    // every position is a label as they should all agree
+    // @TODO: there should be positional masking here ... on the other hand, positions that are not 
+    // in a sentence should always agree (both being 0). Lack of masking affects label count only which is 
+    // probably negligible?
+    numLabels = shape.elements();
   }
-  
-  alignmentLoss = guidedScalar * alignmentLoss; // weigh by scalar
 
-  // every position is a label as they should all agree, see caveat at the top.
-  size_t numLabels = alignment->shape().elements();
-  
   // Create label node, also weigh by scalar so labels and cost are in the same domain.
-  // Fractional label counts are OK
-  return RationalLoss(alignmentLoss, guidedScalar * numLabels);
+  // Fractional label counts are OK. But only if combined as "sum".
+  // @TODO: It is ugly to check the multi-loss type here, but doing this right requires
+  // a substantial rewrite of the multi-loss architecture, which is planned anyways.
+  std::string multiLossType = options->get<std::string>("multi-loss-type", "sum");
+  if (multiLossType == "sum")         // sum of sums
+    return RationalLoss(guidedLossWeight * alignmentLoss, guidedLossWeight * numLabels);
+  else
+    return RationalLoss(guidedLossWeight * alignmentLoss, (float)numLabels);
 }
 
 }  // namespace marian
